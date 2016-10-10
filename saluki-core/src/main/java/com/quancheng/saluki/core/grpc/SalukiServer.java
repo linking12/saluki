51c234f4a913ba2280952318722c83d02046df11
刘石明 <liushiming@quancheng-ec.com>
2016-10-10 22:56:29 +0800

    up

--- saluki-core/src/main/java/com/quancheng/saluki/core/grpc/SalukiServer.java
+++ saluki-core/src/main/java/com/quancheng/saluki/core/grpc/SalukiServer.java
@@ -1,19 +1,13 @@
 package com.quancheng.saluki.core.grpc;
 
-import java.util.Set;
-
-import com.google.common.collect.Sets;
-
 import io.grpc.Server;
 
 public class SalukiServer {
 
-    private static final Set<Server> servers = Sets.newConcurrentHashSet();
+    private Server[] servers;
 
     public SalukiServer(Server... args){
-        for (Server server : args) {
-            servers.add(server);
-        }
+        servers = args;
     }
 
     public void awaitTermination() throws InterruptedException {
