04e0cb594dc5bb05ebcc80b136ca323bdae84417
刘石明 <liushiming@quancheng-ec.com>
2016-10-21 19:00:29 +0800

    up

--- saluki-core/src/main/java/com/quancheng/saluki/core/grpc/cluster/RegistryDirectory.java
+++ saluki-core/src/main/java/com/quancheng/saluki/core/grpc/cluster/RegistryDirectory.java
@@ -4,11 +4,10 @@ import java.net.InetSocketAddress;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Map;
 
 import com.google.api.client.util.Lists;
-import com.google.common.base.Ticker;
-import com.google.common.cache.Cache;
-import com.google.common.cache.CacheBuilder;
+import com.google.common.collect.Maps;
 import com.google.common.net.InetAddresses;
 import com.quancheng.saluki.core.common.SalukiURL;
 import com.quancheng.saluki.core.registry.NotifyListener;
@@ -39,29 +38,23 @@ public interface RegistryDirectory {
 
     public static class Default implements RegistryDirectory {
 
-        private static RegistryDirectory                   directory;
+        private static RegistryDirectory              directory;
 
-        private Cache<SalukiURL, List<ResolvedServerInfo>> resolvedServerInfos;
+        private Map<String, List<ResolvedServerInfo>> resolvedServerInfos;
 
-        private Cache<SalukiURL, NameResolver.Listener>    resolvedListeners;
+        private Map<String, NameResolver.Listener>    resolvedListeners;
 
-        private Registry                                   registry;
+        private Registry                              registry;
 
         public void init(SalukiURL registryUrl) {
             this.registry = RegistryProvider.asFactory().newRegistry(registryUrl);
-            this.resolvedServerInfos = CacheBuilder.newBuilder()//
-                                                   .softValues()//
-                                                   .ticker(Ticker.systemTicker())//
-                                                   .build();
-            this.resolvedListeners = CacheBuilder.newBuilder()//
-                                                 .softValues()//
-                                                 .ticker(Ticker.systemTicker())//
-                                                 .build();
+            this.resolvedServerInfos = Maps.newConcurrentMap();
+            this.resolvedListeners = Maps.newConcurrentMap();
             directory = this;
         }
 
         public void subscribe(SalukiURL subscribeUrl, NameResolver.Listener listener) {
-            resolvedListeners.put(subscribeUrl, listener);
+            resolvedListeners.put(subscribeUrl.getServiceKey(), listener);
             registry.subscribe(subscribeUrl, new NotifyListener() {
 
                 @Override
@@ -80,7 +73,7 @@ public interface RegistryDirectory {
         }
 
         public void remove(SalukiURL subscribeUrl, ResolvedServerInfo serverInfo) {
-            List<ResolvedServerInfo> serverInfos = resolvedServerInfos.getIfPresent(subscribeUrl);
+            List<ResolvedServerInfo> serverInfos = resolvedServerInfos.get(subscribeUrl.getServiceKey());
             List<ResolvedServerInfo> newServerInfos = Lists.newArrayList();
             newServerInfos.addAll(serverInfos);
             newServerInfos.remove(serverInfo);
@@ -88,8 +81,8 @@ public interface RegistryDirectory {
         }
 
         private void notifyLoadBalance(SalukiURL subscribeUrl, List<ResolvedServerInfo> servers) {
-            NameResolver.Listener listener = resolvedListeners.getIfPresent(subscribeUrl);
-            if (servers == null || servers.isEmpty()) {
+            NameResolver.Listener listener = resolvedListeners.get(subscribeUrl.getServiceKey());
+            if (servers != null && !servers.isEmpty()) {
                 listener.onUpdate(Collections.singletonList(servers), Attributes.EMPTY);
             } else {
                 if (listener == null) {
@@ -110,7 +103,7 @@ public interface RegistryDirectory {
                     servers.add(new ResolvedServerInfo(new InetSocketAddress(InetAddresses.forString(ip), port),
                                                        Attributes.EMPTY));
                 }
-                resolvedServerInfos.put(subscribeUrl, servers);
+                resolvedServerInfos.put(subscribeUrl.getServiceKey(), servers);
                 return servers;
             }
             return null;
--- saluki-example/src/main/java/com/quancheng/boot/starter/clientproxy/GreeterServiceController.java
+++ saluki-example/src/main/java/com/quancheng/boot/starter/clientproxy/GreeterServiceController.java
@@ -1,12 +1,23 @@
 package com.quancheng.boot.starter.clientproxy;
 
+import java.net.InetSocketAddress;
+import java.net.SocketAddress;
+import java.util.Map;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.google.common.collect.Maps;
 import com.quancheng.boot.saluki.starter.SalukiReference;
 import com.quancheng.boot.starter.model.HelloReply;
 import com.quancheng.boot.starter.server.GreeterService;
 import com.quancheng.saluki.core.common.RpcContext;
+import com.quancheng.saluki.core.common.SalukiConstants;
+import com.quancheng.saluki.core.common.SalukiURL;
+import com.quancheng.saluki.core.grpc.cluster.RegistryDirectory;
+
+import io.grpc.Attributes;
+import io.grpc.ResolvedServerInfo;
 
 @RestController
 @RequestMapping("/greeter")
@@ -17,7 +28,15 @@ public class GreeterServiceController {
 
     @RequestMapping
     public HelloReply view() {
-
+        Map<String, String> param = Maps.newHashMap();
+        param.put(SalukiConstants.GROUP_KEY, "Default");
+        param.put(SalukiConstants.VERSION_KEY, "1.0.0");
+        SalukiURL url = new SalukiURL(SalukiConstants.DEFATULT_PROTOCOL, "localhost", 0,
+                                      "com.quancheng.boot.starter.server.GreeterService");
+        SocketAddress address = new InetSocketAddress("192.168.99.1", 12201);
+        ResolvedServerInfo serverinfo = new ResolvedServerInfo(address, Attributes.EMPTY);
+        RegistryDirectory.getInstance().remove(url, serverinfo);
+        
         com.quancheng.boot.starter.model.HelloRequest request = new com.quancheng.boot.starter.model.HelloRequest();
         request.setName("liushiming");
         RpcContext.getContext().set("123", "helloworld");
