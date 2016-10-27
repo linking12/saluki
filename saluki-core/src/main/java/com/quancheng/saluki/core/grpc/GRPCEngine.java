be171441bc1057b360dd8f07b003290311f2c3e5
刘石明 <liushiming@quancheng-ec.com>
2016-10-27 23:20:10 +0800

    ca

--- saluki-core/src/main/java/com/quancheng/saluki/core/grpc/GRPCEngine.java
+++ saluki-core/src/main/java/com/quancheng/saluki/core/grpc/GRPCEngine.java
@@ -1,11 +1,15 @@
 package com.quancheng.saluki.core.grpc;
 
+import java.io.File;
 import java.util.Map;
+
+import javax.net.ssl.SSLException;
 
 import com.quancheng.saluki.core.common.SalukiConstants;
 import com.quancheng.saluki.core.common.SalukiURL;
 import com.quancheng.saluki.core.grpc.client.GrpcClientContext;
 import com.quancheng.saluki.core.grpc.client.GrpcProtocolClient;
+import com.quancheng.saluki.core.grpc.exception.RpcFrameworkException;
 import com.quancheng.saluki.core.grpc.interceptor.HeaderClientInterceptor;
 import com.quancheng.saluki.core.grpc.interceptor.HeaderServerInterceptor;
 import com.quancheng.saluki.core.grpc.server.GrpcServerContext;
@@ -15,12 +19,15 @@ import com.quancheng.saluki.core.registry.RegistryProvider;
 import io.grpc.Channel;
 import io.grpc.ClientInterceptors;
 import io.grpc.LoadBalancer;
-import io.grpc.ManagedChannelBuilder;
 import io.grpc.ServerInterceptors;
 import io.grpc.ServerServiceDefinition;
 import io.grpc.inprocess.InProcessChannelBuilder;
 import io.grpc.inprocess.InProcessServerBuilder;
+import io.grpc.netty.NettyChannelBuilder;
 import io.grpc.netty.NettyServerBuilder;
+import io.netty.handler.ssl.SslContext;
+import io.netty.handler.ssl.SslContextBuilder;
+import io.netty.handler.ssl.SslProvider;
 
 public class GRPCEngine {
 
@@ -43,11 +50,12 @@ public class GRPCEngine {
                 if (localProcess) {
                     channel = InProcessChannelBuilder.forName(SalukiConstants.GRPC_IN_LOCAL_PROCESS).build();
                 } else {
-                    channel = ManagedChannelBuilder.forTarget(registryUrl.toJavaURI().toString())//
-                                                   .nameResolverFactory(new SalukiNameResolverProvider(refUrl))//
-                                                   .loadBalancerFactory(buildLoadBalanceFactory())//
-                                                   .usePlaintext(true)//
-                                                   .build();//
+                    channel = NettyChannelBuilder.forTarget(registryUrl.toJavaURI().toString())//
+                                                 .nameResolverFactory(new SalukiNameResolverProvider(refUrl))//
+                                                 .loadBalancerFactory(buildLoadBalanceFactory())//
+                                                 .sslContext(buildClientSslContext())//
+                                                 .usePlaintext(false)//
+                                                 .build();//
                 }
                 return ClientInterceptors.intercept(channel, new HeaderClientInterceptor());
             }
@@ -57,12 +65,25 @@ public class GRPCEngine {
         return context.getGrpcClient();
     }
 
+    private SslContext buildClientSslContext() {
+        try {
+            return SslContextBuilder.forClient()//
+                                    .sslProvider(SslProvider.OPENSSL)//
+                                    .keyManager(new File("cacert.pem"), new File("client.pem"))//
+                                    .build();
+        } catch (SSLException e) {
+            throw new RpcFrameworkException(e);
+        }
+    }
+
     private LoadBalancer.Factory buildLoadBalanceFactory() {
         return SalukiRoundRobinLoadBalanceFactory.getInstance();
     }
 
     public SalukiServer getServer(Map<SalukiURL, Object> providerUrls, int port) throws Exception {
-        final NettyServerBuilder remoteServer = NettyServerBuilder.forPort(port);
+        final NettyServerBuilder remoteServer = NettyServerBuilder.forPort(port)//
+                                                                  .useTransportSecurity(new File("cacert.pem"),
+                                                                                        new File("server.pem"));
         final InProcessServerBuilder injvmServer = InProcessServerBuilder.forName(SalukiConstants.GRPC_IN_LOCAL_PROCESS);
         for (Map.Entry<SalukiURL, Object> entry : providerUrls.entrySet()) {
             SalukiURL providerUrl = entry.getKey();
