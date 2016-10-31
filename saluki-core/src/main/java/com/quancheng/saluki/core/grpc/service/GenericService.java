c7837a1ebc14ee4cd646fbe7611b51329749c980
刘石明 <liushiming@quancheng-ec.com>
2016-10-31 11:07:27 +0800

    up

--- saluki-core/src/main/java/com/quancheng/saluki/core/grpc/service/GenericService.java
+++ saluki-core/src/main/java/com/quancheng/saluki/core/grpc/service/GenericService.java
@@ -2,5 +2,5 @@ package com.quancheng.saluki.core.grpc.service;
 
 public interface GenericService {
 
-    Object $invoke(String serviceName, String method, String[] parameterTypes, Object[] args) throws GenericException;
+    Object $invoke(String serviceName, String method, String[] parameterTypes, Object[] args);
 }
