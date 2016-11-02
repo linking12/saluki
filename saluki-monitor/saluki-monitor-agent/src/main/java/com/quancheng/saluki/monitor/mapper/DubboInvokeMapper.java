fd978a007dac42915098615f1f5049fd06405262
刘石明 <liushiming@quancheng-ec.com>
2016-11-02 14:27:49 +0800

    up

--- saluki-monitor/saluki-monitor-agent/src/main/java/com/quancheng/saluki/monitor/mapper/DubboInvokeMapper.java
+++ saluki-monitor/saluki-monitor-agent/src/main/java/com/quancheng/saluki/monitor/mapper/DubboInvokeMapper.java
@@ -9,13 +9,4 @@ public interface DubboInvokeMapper {
 
     int addDubboInvoke(DubboInvoke dubboInvoke);
 
-    DubboInvoke countDubboInvoke(DubboInvoke dubboInvoke);
-
-    DubboInvoke countDubboInvokeInfo(DubboInvoke dubboInvoke);
-
-    String getMethodsByService(DubboInvoke dubboInvoke);
-
-    DubboInvoke countDubboInvokeSuccessTopTen(DubboInvoke dubboInvoke);
-
-    DubboInvoke countDubboInvokeFailureTopTen(DubboInvoke dubboInvoke);
 }
