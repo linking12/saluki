3840736b38b1ffbd54737246bf08ca4b56b28371
刘石明 <liushiming@quancheng-ec.com>
2016-11-03 21:38:16 +0800

    up

--- saluki-monitor/saluki-monitor-agent/src/main/java/com/quancheng/saluki/monitor/web/SystemController.java
+++ saluki-monitor/saluki-monitor-agent/src/main/java/com/quancheng/saluki/monitor/web/SystemController.java
@@ -1,18 +1,3 @@
-/**
- * Copyright 2006-2015 handu.com
- * <p/>
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * <p/>
- * http://www.apache.org/licenses/LICENSE-2.0
- * <p/>
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package com.quancheng.saluki.monitor.web;
 
 import java.lang.management.ManagementFactory;
@@ -23,35 +8,26 @@ import java.util.List;
 import java.util.Locale;
 import java.util.Map;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.google.common.collect.Maps;
 import com.quancheng.saluki.core.utils.NetUtils;
 
-/**
- * SystemController
- *
- * @author Jinkai.Ma
- */
-@Controller
-@RequestMapping("/salukiMonitor/system")
+@RestController
+@RequestMapping("/salukiMonitor")
 public class SystemController {
 
     private SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS Z");
 
-    @RequestMapping(method = RequestMethod.GET)
+    @RequestMapping(value = "/system", method = RequestMethod.GET)
     public Map<String, Object> system() {
         List<String[]> rows = new ArrayList<String[]>();
-
-        rows.add(new String[] { "Version", "2.8.4" });
-
+        rows.add(new String[] { "Saluki_Version", "1.1" });
         String address = NetUtils.getLocalHost();
         rows.add(new String[] { "Host", NetUtils.getHostName(address) + "/" + address });
-
         rows.add(new String[] { "OS", System.getProperty("os.name") + " " + System.getProperty("os.version") });
-
         rows.add(new String[] { "JVM",
                                 System.getProperty("java.runtime.name") + " "
                                        + System.getProperty("java.runtime.version") + ",<br/>"
