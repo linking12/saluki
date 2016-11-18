a475fdb5fac56c57f5a2f7c4c23a40383f640ff0
刘石明 <liushiming@quancheng-ec.com>
2016-11-18 17:17:48 +0800

    up

--- saluki-monitor/saluki-monitor-web/src/main/java/com/quancheng/saluki/monitor/web/controller/SalukiMonitorController.java
+++ saluki-monitor/saluki-monitor-web/src/main/java/com/quancheng/saluki/monitor/web/controller/SalukiMonitorController.java
@@ -1,8 +1,5 @@
 package com.quancheng.saluki.monitor.web.controller;
 
-import java.util.List;
-import java.util.Map;
-
 import javax.annotation.PostConstruct;
 
 import org.apache.http.HttpResponse;
@@ -16,29 +13,23 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.google.gson.Gson;
-import com.google.gson.reflect.TypeToken;
-import com.quancheng.saluki.monitor.domain.SalukiInvokeStatistics;
-
 @RestController
 @RequestMapping(value = "monitor")
-public class SalukiMonitorDataController {
+public class SalukiMonitorController {
 
-    private Logger     log  = Logger.getLogger(ApplicationController.class);
+    private Logger     log = Logger.getLogger(ApplicationController.class);
 
     private HttpClient httpClient;
-
-    private final Gson gson = new Gson();
 
     @PostConstruct
     public void init() {
         httpClient = HttpClientBuilder.create().build();
     }
 
-    @RequestMapping(method = RequestMethod.GET)
-    public List<SalukiInvokeStatistics> data(@RequestParam(value = "ip", required = true) String ip,
-                                             @RequestParam(value = "port", required = true) String port,
-                                             @RequestParam(value = "service", required = true) String service) throws Exception {
+    @RequestMapping(value = "data", method = RequestMethod.GET)
+    public String data(@RequestParam(value = "ip", required = true) String ip,
+                       @RequestParam(value = "port", required = true) String port,
+                       @RequestParam(value = "service", required = true) String service) throws Exception {
         log.info("Return all monitor data");
         String monitordataUrl = "http://" + ip + ":" + port + "/salukiMonitor/data?service=" + service;
         HttpGet request = new HttpGet(monitordataUrl);
@@ -46,20 +37,15 @@ public class SalukiMonitorDataController {
         request.addHeader("Accept", "application/json");
         try {
             HttpResponse httpResponse = httpClient.execute(request);
-            String response = EntityUtils.toString(httpResponse.getEntity());
-
-            List<SalukiInvokeStatistics> statistics = gson.fromJson(response,
-                                                                    new TypeToken<List<SalukiInvokeStatistics>>() {
-                                                                    }.getType());
-            return statistics;
+            return EntityUtils.toString(httpResponse.getEntity());
         } catch (Exception e) {
             throw e;
         }
     }
 
-    @RequestMapping(method = RequestMethod.GET)
-    public Map<String, Object> system(@RequestParam(value = "ip", required = true) String ip,
-                                      @RequestParam(value = "port", required = true) String port) throws Exception {
+    @RequestMapping(value = "system", method = RequestMethod.GET)
+    public String system(@RequestParam(value = "ip", required = true) String ip,
+                         @RequestParam(value = "port", required = true) String port) throws Exception {
         log.info("Return all monitor data");
         String monitordataUrl = "http://" + ip + ":" + port + "/salukiMonitor/system";
         HttpGet request = new HttpGet(monitordataUrl);
@@ -68,9 +54,7 @@ public class SalukiMonitorDataController {
         try {
             HttpResponse httpResponse = httpClient.execute(request);
             String response = EntityUtils.toString(httpResponse.getEntity());
-            Map<String, Object> system = gson.fromJson(response, new TypeToken<Map<String, Object>>() {
-            }.getType());
-            return system;
+            return response;
         } catch (Exception e) {
             throw e;
         }
