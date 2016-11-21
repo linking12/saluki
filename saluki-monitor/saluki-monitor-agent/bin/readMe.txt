b2626d8aa9be83b3d504d62fb05759593278b60c
刘石明 <liushiming@quancheng-ec.com>
2016-11-21 22:45:20 +0800

    up

---
+++ saluki-monitor/saluki-monitor-agent/bin/readMe.txt
@@ -0,0 +1,10 @@
+1:统计时间段的Qpc和ART
+http://localhost:8787/salukiMonitor/charts?service=com.quancheng.examples.service.HelloService&from=2016-11-2 00:00:00&to=2016-11-3 23:59:59
+2:top10成功和top10失败的
+http://localhost:8787/salukiMonitor/index?from=2016-11-2%2000:00:00&to=2016-11-4%2023:59:59
+3:统计每一个方法平均耗时
+http://localhost:8787/salukiMonitor/statistics?service=com.quancheng.examples.service.HelloService&from=2016-11-2%2000:00:00&to=2016-11-4%2023:59:59
+4:查询所有
+http://localhost:8787/salukiMonitor/all
+5:查询系统信息
+http://localhost:8787/salukiMonitor/system
---
+++ saluki-monitor/saluki-monitor-agent/build/resources/main/sql/create-db.sql
@@ -0,0 +1,23 @@
+DROP TABLE IF EXISTS `saluki_invoke_statistics`;
+CREATE TABLE `saluki_invoke_statistics` (
+  `id` varchar(255) PRIMARY KEY,
+  `invokeDate` datetime DEFAULT NULL,
+  `service` varchar(255) DEFAULT NULL,
+  `method` varchar(255) DEFAULT NULL,
+  `consumer` varchar(255) DEFAULT NULL,
+  `provider` varchar(255) DEFAULT NULL,
+  `type` varchar(255) DEFAULT '',
+  `success` int(11) DEFAULT NULL,
+  `failure` int(11) DEFAULT NULL,
+  `elapsed` int(11) DEFAULT NULL,
+  `concurrent` int(11) DEFAULT NULL,
+  `maxInput` int(11) DEFAULT NULL,
+  `maxOutput` int(11) DEFAULT NULL,
+  `maxElapsed` double DEFAULT NULL,
+  `maxConcurrent` int(11) DEFAULT NULL,
+  `input` double DEFAULT NULL,
+  `output` double DEFAULT NULL,
+  `tps` double DEFAULT NULL,
+  `kbps` double DEFAULT NULL,
+  PRIMARY KEY (`id`)
+);
