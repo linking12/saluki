7557ff98ceb5a50cf092ffb8c81e2a2c2aaa371d
刘石明 <liushiming@quancheng-ec.com>
2016-11-22 11:59:27 +0800

    up

--- saluki-example/saluki-example-client/src/main/java/com/quancheng/saluki/example/client/proxy/ProxyServiceController.java
+++ saluki-example/saluki-example-client/src/main/java/com/quancheng/saluki/example/client/proxy/ProxyServiceController.java
@@ -1,5 +1,11 @@
 package com.quancheng.saluki.example.client.proxy;
 
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+import javax.annotation.PostConstruct;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,6 +14,7 @@ import com.quancheng.examples.model.hello.HelloReply;
 import com.quancheng.examples.model.hello.HelloRequest;
 import com.quancheng.examples.service.HelloService;
 import com.quancheng.saluki.core.common.RpcContext;
+import com.quancheng.saluki.core.utils.NamedThreadFactory;
 import com.quancheng.test.model.user.UserCreateRequest;
 import com.quancheng.test.model.user.UserCreateResponse;
 
@@ -21,8 +28,28 @@ public class ProxyServiceController {
     @SalukiReference(service = "com.quancheng.test.service.UserService", group = "Default", version = "1.0.0")
     private com.quancheng.test.service.UserService userService;
 
+    private final ScheduledExecutorService         scheduledExecutorService = Executors.newScheduledThreadPool(3,
+                                                                                                               new NamedThreadFactory("scheduleTest",
+                                                                                                                                      true));
+
+    @PostConstruct
+    public void init() {
+        scheduledExecutorService.scheduleAtFixedRate(new Runnable() {
+
+            @Override
+            public void run() {
+                call();
+
+            }
+        }, 0, 30, TimeUnit.MINUTES);
+    }
+
     @RequestMapping("/hello")
     public HelloReply hello() {
+        return call();
+    }
+
+    private HelloReply call() {
         HelloRequest request = new HelloRequest();
         request.setName("liushiming");
         RpcContext.getContext().set("123", "helloworld");
