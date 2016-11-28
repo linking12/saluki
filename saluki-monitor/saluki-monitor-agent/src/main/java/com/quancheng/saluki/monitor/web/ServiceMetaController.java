810a5fba66f3106021d12ab430b2990f09d21d79
刘石明 <liushiming@quancheng-ec.com>
2016-11-28 22:24:02 +0800

    up

--- saluki-monitor/saluki-monitor-agent/src/main/java/com/quancheng/saluki/monitor/web/ServiceMetaController.java
+++ saluki-monitor/saluki-monitor-agent/src/main/java/com/quancheng/saluki/monitor/web/ServiceMetaController.java
@@ -8,6 +8,9 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.google.gson.Gson;
+import com.quancheng.boot.saluki.starter.SalukiReference;
+import com.quancheng.saluki.core.grpc.service.GenericService;
 import com.quancheng.saluki.core.utils.ReflectUtil;
 import com.taobao.jaket.Jaket;
 import com.taobao.jaket.model.MethodDefinition;
@@ -17,6 +20,11 @@ import com.taobao.jaket.model.TypeDefinition;
 @RestController
 @RequestMapping("/serviceMeasure")
 public class ServiceMetaController {
+
+    private final Gson     gson = new Gson();
+
+    @SalukiReference(service = "com.quancheng.saluki.core.grpc.service.GenericService", group = "Default", version = "1.0.0")
+    private GenericService genricService;
 
     @RequestMapping(value = "/getAllMethod", method = RequestMethod.GET)
     public List<MethodDefinition> getAllMethod(@RequestParam(value = "service", required = true) String service) throws ClassNotFoundException {
@@ -57,6 +65,27 @@ public class ServiceMetaController {
         } catch (ClassNotFoundException e) {
             throw e;
         }
+    }
+
+    @RequestMapping(value = "/testService", method = RequestMethod.POST)
+    public Object testService(@RequestParam(value = "group", required = true) String group,
+                              @RequestParam(value = "version", required = true) String version,
+                              @RequestParam(value = "service", required = true) String service,
+                              @RequestParam(value = "method", required = true) String method,
+                              @RequestParam(value = "parameterType", required = true) String parameterType,
+                              @RequestParam(value = "returnType", required = true) String returnType,
+                              @RequestParam(value = "parameter", required = true) String parameter) throws ClassNotFoundException {
+        try {
+            Class<?> requestClass = ReflectUtil.name2class(parameterType);
+            Object request = gson.fromJson(parameter, requestClass);
+            String[] paramTypes = new String[] { parameterType, returnType };
+            Object[] args = new Object[] { request };
+            Object reply = genricService.$invoke(service, group, version, method, paramTypes, args);
+            return reply;
+        } catch (ClassNotFoundException e) {
+            throw e;
+        }
+
     }
 
 }
