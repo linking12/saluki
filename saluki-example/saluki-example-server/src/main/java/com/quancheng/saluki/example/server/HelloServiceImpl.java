33ca6337edbf0f2cd2057bc7e332a9f9f2b33e27
刘石明 <liushiming@quancheng-ec.com>
2016-12-02 19:21:03 +0800

    up

--- saluki-example/saluki-example-server/src/main/java/com/quancheng/saluki/example/server/HelloServiceImpl.java
+++ saluki-example/saluki-example-server/src/main/java/com/quancheng/saluki/example/server/HelloServiceImpl.java
@@ -1,6 +1,5 @@
 package com.quancheng.saluki.example.server;
 
-import com.google.common.base.Preconditions;
 import com.quancheng.boot.saluki.starter.SalukiService;
 import com.quancheng.examples.model.hello.HelloReply;
 import com.quancheng.examples.model.hello.HelloRequest;
@@ -13,8 +12,8 @@ public class HelloServiceImpl implements HelloService {
     public HelloReply sayHello(HelloRequest request) {
         HelloReply reply = new HelloReply();
         reply.setMessage(request.getName());
-        int registryPort = 0;
-        Preconditions.checkState(registryPort != 0, "RegistryPort can not be null", registryPort);
+        //int registryPort = 0;
+        //Preconditions.checkState(registryPort != 0, "RegistryPort can not be null", registryPort);
         return reply;
     }
 
