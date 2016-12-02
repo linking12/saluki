7c7cd83be63631a15c26757a0e46d4579496f5bb
刘石明 <liushiming@quancheng-ec.com>
2016-12-02 10:44:44 +0800

    up

--- saluki-example/saluki-example-server/bin/start.sh
+++ saluki-example/saluki-example-server/bin/start.sh
@@ -12,4 +12,7 @@ JAVA_OPTS="${JAVA_OPTS} -Djava.awt.headless=true"
 JAVA_OPTS="${JAVA_OPTS} -Dsun.net.client.defaultConnectTimeout=10000"
 JAVA_OPTS="${JAVA_OPTS} -Dsun.net.client.defaultReadTimeout=30000"
 JAVA_OPTS="${JAVA_OPTS} -DAPP_NAME=${APP_NAME}"
+JAVA_OPTS="${JAVA_OPTS} -Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.port=60001 -Djava.rmi.server.hostname=192.168。2.14"
+JAVA_OPTS="${JAVA_OPTS} -Dcom.sun.management.jmxremote.authenticate=false"
+JAVA_OPTS="${JAVA_OPTS} -Dcom.sun.management.jmxremote.ssl=false"
 java -agentlib:jdwp=transport=dt_socket,server=y,address=8002,suspend=n -Djava.security.egd=file:/dev/./urandom $JAVA_OPTS -jar ./app.jar
