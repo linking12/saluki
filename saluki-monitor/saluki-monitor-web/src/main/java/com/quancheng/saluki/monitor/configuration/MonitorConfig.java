6e49261a60030ee0fc2b6405331b9db99dae9d70
刘石明 <liushiming@quancheng-ec.com>
2016-12-13 14:12:53 +0800

    up

--- saluki-monitor/saluki-monitor-web/src/main/java/com/quancheng/saluki/monitor/configuration/MonitorConfig.java
+++ saluki-monitor/saluki-monitor-web/src/main/java/com/quancheng/saluki/monitor/configuration/MonitorConfig.java
@@ -1,30 +1,11 @@
 package com.quancheng.saluki.monitor.configuration;
 
-import java.sql.SQLException;
-
-import javax.sql.DataSource;
-
-import org.apache.ibatis.session.SqlSessionFactory;
-import org.mybatis.spring.SqlSessionFactoryBean;
-import org.mybatis.spring.SqlSessionTemplate;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
-import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
-import org.springframework.core.io.support.ResourcePatternResolver;
-import org.springframework.jdbc.datasource.DataSourceTransactionManager;
-import org.springframework.transaction.PlatformTransactionManager;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
-import org.springframework.transaction.annotation.TransactionManagementConfigurer;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
-
-import com.alibaba.druid.pool.DruidDataSource;
-import com.alibaba.druid.support.http.StatViewServlet;
-import com.alibaba.druid.support.http.WebStatFilter;
 
 @Configuration
 public class MonitorConfig {
@@ -53,73 +34,4 @@ public class MonitorConfig {
         return new CorsFilter(source);
     }
 
-    @Bean
-    public ServletRegistrationBean druidServlet() {
-        return new ServletRegistrationBean(new StatViewServlet(), "/druid/*");
-    }
-
-    @Bean
-    public DataSource druidDataSource(@Value("${spring.datasource.driverClassName}") String driver,
-                                      @Value("${spring.datasource.url}") String url,
-                                      @Value("${spring.datasource.username}") String username,
-                                      @Value("${spring.datasource.password}") String password) {
-        DruidDataSource druidDataSource = new DruidDataSource();
-        druidDataSource.setDriverClassName(driver);
-        druidDataSource.setUrl(url);
-        druidDataSource.setUsername(username);
-        druidDataSource.setPassword(password);
-        try {
-            druidDataSource.setFilters("stat, wall");
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        return druidDataSource;
-    }
-
-    @Bean(name = "sqlSessionFactory")
-    public SqlSessionFactory sqlSessionFactoryBean(DataSource dataSource) {
-        SqlSessionFactoryBean bean = new SqlSessionFactoryBean();
-        bean.setDataSource(dataSource);
-        bean.setTypeAliasesPackage("com.quancheng.saluki.monitor");
-        ResourcePatternResolver resolver = new PathMatchingResourcePatternResolver();
-        try {
-            bean.setMapperLocations(resolver.getResources("classpath:mappers/*.xml"));
-            return bean.getObject();
-        } catch (Exception e) {
-            e.printStackTrace();
-            throw new RuntimeException(e);
-        }
-    }
-
-    @Bean
-    public SqlSessionTemplate sqlSessionTemplate(SqlSessionFactory sqlSessionFactory) {
-        return new SqlSessionTemplate(sqlSessionFactory);
-    }
-
-    @Configuration
-    @EnableTransactionManagement
-    public static class TransactionConfig implements TransactionManagementConfigurer {
-
-        private DataSource dataSource;
-
-        public TransactionConfig(DataSource dataSource){
-            this.dataSource = dataSource;
-        }
-
-        @Bean
-        @Override
-        public PlatformTransactionManager annotationDrivenTransactionManager() {
-            return new DataSourceTransactionManager(dataSource);
-        }
-
-    }
-
-    @Bean
-    public FilterRegistrationBean filterRegistrationBean() {
-        FilterRegistrationBean filterRegistrationBean = new FilterRegistrationBean();
-        filterRegistrationBean.setFilter(new WebStatFilter());
-        filterRegistrationBean.addUrlPatterns("/*");
-        filterRegistrationBean.addInitParameter("exclusions", "*.js,*.gif,*.jpg,*.png,*.css,*.ico,/druid/*");
-        return filterRegistrationBean;
-    }
 }
