d9d0c807e8bf921cdba6c693e506f44d6b5034ee
刘石明 <liushiming@quancheng-ec.com>
2016-12-15 14:25:40 +0800

    fix bug

--- saluki-plugin/src/main/groovy/com/quancheng/saluki/plugin/SalukiRpcPlugin.groovy
+++ saluki-plugin/src/main/groovy/com/quancheng/saluki/plugin/SalukiRpcPlugin.groovy
@@ -113,14 +113,18 @@ class SalukiRpcPlugin implements Plugin<Project> {
                     nameFilter: ~/.*\.proto/
             ) {
                 def message = [:]
+                def enummessage = [:]
                 def methodClassName = [:]
                 def messagepb = [:]
                 List files = getImportFiles(it.path, project, [])
+                def importedFileClassNamePackageNameMap = [:]
                 files.reverse().each { file ->
                     def packageName1 = ""
                     def java_outer_classname = ""
                     def massageName = ""
+                    def enumName = ""
                     def massageParamList = []
+                    def enumParamList = []
                     new File(file).eachLine { line, nb ->
                         def java_package1 = line =~ /^option\s+java_package\s*=\s*\"(.+)\"/
                         if (java_package1.size() > 0) {
@@ -130,15 +134,26 @@ class SalukiRpcPlugin implements Plugin<Project> {
                         if (java_outer_classname_filter.size() > 0) {
                             java_outer_classname = java_outer_classname_filter[0][1]
                         }
-                        def param_filter = line =~ /^message\s+(.+)\{/
-                        if (param_filter.size() > 0) {
-                            massageName = packageName1 + "." + java_outer_classname.toLowerCase() + "." + param_filter[0][1].trim()
-                            methodClassName.put(packageName1 + "." + param_filter[0][1].trim(), packageName1 + "." + java_outer_classname.toLowerCase() + "." + param_filter[0][1].trim())
+                        def parammessage_filter = line =~ /^message\s+(.+)\{/
+                        def paramenum_filter = line =~ /^enum\s+(.+)\{/
+                        if (parammessage_filter.size() > 0) {
+                            massageName = packageName1 + "." + java_outer_classname.toLowerCase() + "." + parammessage_filter[0][1].trim()
+                            methodClassName.put(packageName1 + "." + parammessage_filter[0][1].trim(), packageName1 + "." + java_outer_classname.toLowerCase() + "." + parammessage_filter[0][1].trim())
+                        }
+                        if(paramenum_filter.size() > 0){
+                            enumName = packageName1 + "." + java_outer_classname.toLowerCase() + "." + paramenum_filter[0][1].trim()
+                            methodClassName.put(packageName1 + "." + paramenum_filter[0][1].trim(), packageName1 + "." + java_outer_classname.toLowerCase() + "." + paramenum_filter[0][1].trim())
                         }
                         def massageParam = line =~ /^\s*(.+)\s+(.+)\s*=\s*(\d+)/
+                        def enumParam = line =~ /^\s*(.+)\s*=\s*(\d+)/
                         if (massageParam.size() > 0) {
                             if (massageName) {
                                 massageParamList.add(massageParam[0])
+                            }
+                        }
+                        if (enumParam.size() > 0) {
+                            if (enumName){
+                                enumParamList.add(enumParam[0])
                             }
                         }
                         def massageParamEnd = line =~ /^\s*\}$/
@@ -149,7 +164,57 @@ class SalukiRpcPlugin implements Plugin<Project> {
                                 massageParamList = []
                                 massageName = ""
                             }
+                            if (enumName){
+                                enummessage.put(enumName, enumParamList)
+                                enumParamList = []
+                                enumName = ""
+                            }
                         }
+                    }
+                }
+                if(enummessage) {
+                    enummessage.each { enumData ->
+                       List packageName = enumData.getKey().split("\\.")
+                       if (packageName.size() > 2){
+                            def str = packageName[0..(packageName.size() - 1)].join("/")
+                            def path = project.projectDir.getPath() + "/build/generated/source/proto/main/java/" + str + ".java"
+                            def file = new File(path)
+                            file.parentFile.mkdirs()
+                            def printWriter = file.newPrintWriter()
+                            importedFileClassNamePackageNameMap.put(packageName[(packageName.size() - 1)],packageName[0..(packageName.size() - 2)].join("."));
+                            printWriter.write("package " + packageName[0..(packageName.size() - 2)].join(".") + ";")
+                            printWriter.write('\n')
+                            printWriter.write('\n')
+                            printWriter.write('\n')
+                            printWriter.write('public enum ' + packageName[(packageName.size() - 1)] + ' { \n')
+                            enumData.getValue().eachWithIndex { param,index ->
+                                 if(index!=enumData.getValue().size-1){
+                                  printEnumParam(false,param[1],param[2],printWriter)
+                                 }else{
+                                  printEnumParam(true,param[1],param[2],printWriter)
+                                 }
+                            }
+                           
+                            enumData.getValue().eachWithIndex { param,index ->
+                                 if(index==0){
+                                    printEnumMethod(packageName[(packageName.size() - 1)],printWriter);
+                                    printWriter.write('\n')
+                                    printWriter.write('    public static '+packageName[(packageName.size() - 1)]+' forNumber(Integer value) {\n')
+                                    printWriter.write('        switch (value) {\n')
+                                 }
+                                 printWriter.write('                case '+param[2]+":\n")
+                                 printWriter.write('                     return '+param[1]+';\n')
+                                 if(index==enumData.getValue().size-1){
+                                    printWriter.write('                default:\n')
+                                    printWriter.write('                     return null;\n')
+                                    printWriter.write('        }\n')
+                                    printWriter.write('    }\n')
+                                 }
+                            }
+                            printWriter.write('}')
+                            printWriter.flush()
+                            printWriter.close()
+                       }
                     }
                 }
                 if (message) {
@@ -161,6 +226,7 @@ class SalukiRpcPlugin implements Plugin<Project> {
                             def file = new File(path)
                             file.parentFile.mkdirs()
                             def printWriter = file.newPrintWriter()
+                            importedFileClassNamePackageNameMap.put(packageName[(packageName.size() - 1)],packageName[0..(packageName.size() - 2)].join("."));
                             printWriter.write("package " + packageName[0..(packageName.size() - 2)].join(".") + ";")
                             printWriter.write('\n')
                             printWriter.write('\n')
@@ -175,72 +241,79 @@ class SalukiRpcPlugin implements Plugin<Project> {
                                     printGet("String", param[2], printWriter)
                                     printSet("String", param[2], printWriter)
                                 }
-                                if (param[1].trim() == "int32") {
+                                else if (param[1].trim() == "int32") {
                                     printParam("Integer", param[2], printWriter)
                                     printGet("Integer", param[2], printWriter)
                                     printSet("Integer", param[2], printWriter)
                                 }
-                                if (param[1].trim() == "int64") {
+                                else if (param[1].trim() == "int64") {
                                     printParam("Long", param[2], printWriter)
                                     printGet("Long", param[2], printWriter)
                                     printSet("Long", param[2], printWriter)
                                 }
-                                if (param[1].trim() == "bool") {
+                                else if (param[1].trim() == "bool") {
                                     printParam("Boolean", param[2], printWriter)
                                     printGet("Boolean", param[2], printWriter)
                                     printSet("Boolean", param[2], printWriter)
                                 }
-                                if (param[1].trim() == "double") {
+                                else if (param[1].trim() == "double") {
                                     printParam("Double", param[2], printWriter)
                                     printGet("Double", param[2], printWriter)
                                     printSet("Double", param[2], printWriter)
                                 }
-                                if (param[1].trim() == "float") {
+                                else if (param[1].trim() == "float") {
                                     printParam("Float", param[2], printWriter)
                                     printGet("Float", param[2], printWriter)
                                     printSet("Float", param[2], printWriter)
-                                }
-                                if (methodClassName[param[1]] != null) {
-                                    printParam(methodClassName[param[1]], param[2], printWriter)
-                                    printGet(methodClassName[param[1]], param[2], printWriter)
-                                    printSet(methodClassName[param[1]], param[2], printWriter)
-                                }
-                                if (param[1].trim().split(" ").size() == 2) {
-                                    if (param[1].trim().split(" ")[0] == "repeated") {
-                                        if (param[1].trim().split(" ")[1].trim() == "string") {
-                                            printParam("java.util.ArrayList<String>", param[2], printWriter)
-                                            printGet("java.util.ArrayList<String>", param[2], printWriter)
-                                            printSet("java.util.ArrayList<String>", param[2], printWriter)
-                                        } else if (param[1].trim().split(" ")[1].trim() == "int32") {
-                                            printParam("java.util.ArrayList<Integer>", param[2], printWriter)
-                                            printGet("java.util.ArrayList<Integer>", param[2], printWriter)
-                                            printSet("java.util.ArrayList<Integer>", param[2], printWriter)
-                                        } else if (param[1].trim().split(" ")[1].trim() == "int64") {
-                                            printParam("java.util.ArrayList<Long>", param[2], printWriter)
-                                            printGet("java.util.ArrayList<Long>", param[2], printWriter)
-                                            printSet("java.util.ArrayList<Long>", param[2], printWriter)
-                                        } else if (param[1].trim().split(" ")[1].trim() == "bool") {
-                                            printParam("java.util.ArrayList<Boolean>", param[2], printWriter)
-                                            printGet("java.util.ArrayList<Boolean>", param[2], printWriter)
-                                            printSet("java.util.ArrayList<Boolean>", param[2], printWriter)
-                                        } else {
-                                            if (param[1].trim().split(" ")[1].split("\\.").size() > 1) {
-                                                printParam("java.util.ArrayList<" + methodClassName[param[1].trim().split(" ")[1]] + ">", param[2], printWriter)
-                                                printGet("java.util.ArrayList<" + methodClassName[param[1].trim().split(" ")[1]] + ">", param[2], printWriter)
-                                                printSet("java.util.ArrayList<" + methodClassName[param[1].trim().split(" ")[1]] + ">", param[2], printWriter)
-                                            } else {
-                                                printParam("java.util.ArrayList<" + param[1].trim().split(" ")[1] + ">", param[2], printWriter)
-                                                printGet("java.util.ArrayList<" + param[1].trim().split(" ")[1] + "> ", param[2], printWriter)
-                                                printSet("java.util.ArrayList<" + param[1].trim().split(" ")[1] + "> ", param[2], printWriter)
-                                            }
-                                        }//end param[1].trim().split(" ")[1].trim() == "string"
-                                    }//param[1].trim().split(" ")[0] == "repeated"    
-                                } 
-                                def mapMatch = param[1].trim() =~ /^(map){1}(.*)/
-                                if(mapMatch.size()>0){
-                                    printParam("java.util.Map<String,String>", param[2], printWriter)
-                                    printGet("java.util.Map<String,String> ", param[2], printWriter)
-                                    printSet("java.util.Map<String,String> ", param[2], printWriter)
+                                }else {
+                                    if (methodClassName[param[1]] != null) {
+	                                    printParam(methodClassName[param[1]], param[2], printWriter)
+	                                    printGet(methodClassName[param[1]], param[2], printWriter)
+	                                    printSet(methodClassName[param[1]], param[2], printWriter)
+                                     }
+                                    else if (param[1].trim().split(" ").size() == 2) {
+	                                    if (param[1].trim().split(" ")[0] == "repeated") {
+	                                        if (param[1].trim().split(" ")[1].trim() == "string") {
+	                                            printParam("java.util.ArrayList<String>", param[2], printWriter)
+	                                            printGet("java.util.ArrayList<String>", param[2], printWriter)
+	                                            printSet("java.util.ArrayList<String>", param[2], printWriter)
+	                                        } else if (param[1].trim().split(" ")[1].trim() == "int32") {
+	                                            printParam("java.util.ArrayList<Integer>", param[2], printWriter)
+	                                            printGet("java.util.ArrayList<Integer>", param[2], printWriter)
+	                                            printSet("java.util.ArrayList<Integer>", param[2], printWriter)
+	                                        } else if (param[1].trim().split(" ")[1].trim() == "int64") {
+	                                            printParam("java.util.ArrayList<Long>", param[2], printWriter)
+	                                            printGet("java.util.ArrayList<Long>", param[2], printWriter)
+	                                            printSet("java.util.ArrayList<Long>", param[2], printWriter)
+	                                        } else if (param[1].trim().split(" ")[1].trim() == "bool") {
+	                                            printParam("java.util.ArrayList<Boolean>", param[2], printWriter)
+	                                            printGet("java.util.ArrayList<Boolean>", param[2], printWriter)
+	                                            printSet("java.util.ArrayList<Boolean>", param[2], printWriter)
+	                                        } else {
+	                                            if (param[1].trim().split(" ")[1].split("\\.").size() > 1) {
+	                                                printParam("java.util.ArrayList<" + methodClassName[param[1].trim().split(" ")[1]] + ">", param[2], printWriter)
+	                                                printGet("java.util.ArrayList<" + methodClassName[param[1].trim().split(" ")[1]] + ">", param[2], printWriter)
+	                                                printSet("java.util.ArrayList<" + methodClassName[param[1].trim().split(" ")[1]] + ">", param[2], printWriter)
+	                                            } else {
+	                                                printParam("java.util.ArrayList<" + param[1].trim().split(" ")[1] + ">", param[2], printWriter)
+	                                                printGet("java.util.ArrayList<" + param[1].trim().split(" ")[1] + "> ", param[2], printWriter)
+	                                                printSet("java.util.ArrayList<" + param[1].trim().split(" ")[1] + "> ", param[2], printWriter)
+	                                            }
+	                                        }//end param[1].trim().split(" ")[1].trim() == "string"
+	                                    }//param[1].trim().split(" ")[0] == "repeated"    
+                                    }else{
+                                        def mapMatch = param[1].trim() =~ /^(map){1}(.*)/
+		                                if(mapMatch.size()>0){
+		                                    printParam("java.util.Map<String,String>", param[2], printWriter)
+		                                    printGet("java.util.Map<String,String> ", param[2], printWriter)
+		                                    printSet("java.util.Map<String,String> ", param[2], printWriter)
+		                                }else{
+		                                    def type = importedFileClassNamePackageNameMap.get(param[1].trim())+"."+param[1].trim();
+		                                    printParam(type, param[2], printWriter)
+		                                    printGet(type, param[2], printWriter)
+		                                    printSet(type, param[2], printWriter)
+		                                }
+                                    } 
                                 }
                             }
                             printWriter.write("} \n")
@@ -261,6 +334,25 @@ class SalukiRpcPlugin implements Plugin<Project> {
             }
         }
         return files
+    }
+    void printEnumMethod(enumName,printWriter){
+       printWriter.write("\n")
+       printWriter.write("    private final int value;\n")
+       printWriter.write("    private "+ enumName+"(int value){\n")
+       printWriter.write("        this.value = value;\n")
+       printWriter.write("    }\n")
+       printWriter.write("    public final int getNumber(){\n")
+       printWriter.write("        return value;\n")
+       printWriter.write("    }\n")
+    }
+    
+    void printEnumParam(isEnd,name,number,printWriter) {
+        printWriter.write("\n")
+        if(isEnd){
+          printWriter.write(name+"("+ number +")" + ";\n")
+        }else{
+          printWriter.write(name+"("+ number +")" + ",\n")
+        }
     }
     void printParam(type, name, printWriter) {
         printWriter.write("\n")
