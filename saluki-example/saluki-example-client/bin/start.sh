b443aacee148f9f43c72966f17e84d550a617062
刘石明 <liushiming@quancheng-ec.com>
2016-12-22 10:22:35 +0800

    up

---
+++ saluki-example/saluki-example-client/bin/start.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+export LANG=zh_CN.UTF-8
+app_prefix=${APP_NAME}-`hostname`
+JAVA_OPTS="-server -Xss256k $JAVA_OPTS"
+JAVA_OPTS="${JAVA_OPTS} -XX:SurvivorRatio=10"
+JAVA_OPTS="${JAVA_OPTS} -XX:+UseConcMarkSweepGC  -XX:CMSMaxAbortablePrecleanTime=5000 -XX:+CMSClassUnloadingEnabled -XX:CMSInitiatingOccupancyFraction=80"
+JAVA_OPTS="${JAVA_OPTS} -XX:+UseCMSInitiatingOccupancyOnly"
+JAVA_OPTS="${JAVA_OPTS} -XX:+DisableExplicitGC"
+JAVA_OPTS="${JAVA_OPTS} -verbose:gc -Xloggc:/root/logs/${app_prefix}-gc.log -XX:+PrintGCDetails -XX:+PrintGCDateStamps"
+JAVA_OPTS="${JAVA_OPTS} -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=/root/logs/${app_prefix}-java.hprof"
+JAVA_OPTS="${JAVA_OPTS} -Djava.awt.headless=true"
+JAVA_OPTS="${JAVA_OPTS} -Dsun.net.client.defaultConnectTimeout=10000"
+JAVA_OPTS="${JAVA_OPTS} -Dsun.net.client.defaultReadTimeout=30000"
+JAVA_OPTS="${JAVA_OPTS} -DAPP_NAME=${APP_NAME}"
+JAVA_OPTS="${JAVA_OPTS} -Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.port=60002 -Djava.rmi.server.hostname=192.168.2.14"
+JAVA_OPTS="${JAVA_OPTS} -Dcom.sun.management.jmxremote.authenticate=false"
+JAVA_OPTS="${JAVA_OPTS} -Dcom.sun.management.jmxremote.ssl=false"
+java -agentlib:jdwp=transport=dt_socket,server=y,address=8003,suspend=n -Djava.security.egd=file:/dev/./urandom $JAVA_OPTS -jar ./app.jar
