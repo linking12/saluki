88561a76f4066741490e1e21c97f7f177ee8d8fe
刘石明 <liushiming@quancheng-ec.com>
2016-12-28 22:37:11 +0800

    add service router

--- saluki-core/src/main/java/com/quancheng/saluki/core/grpc/router/internal/ConditonRouter.java
+++ saluki-core/src/main/java/com/quancheng/saluki/core/grpc/router/internal/ConditonRouter.java
@@ -5,20 +5,20 @@
  * use it only in accordance with the terms of the license agreement you entered
  * into with Quancheng-ec.com.
  */
-package com.quancheng.saluki.core.grpc.route.internal;
+package com.quancheng.saluki.core.grpc.router.internal;
 
 import java.util.List;
 
 import com.quancheng.saluki.core.common.GrpcURL;
-import com.quancheng.saluki.core.grpc.route.GrpcRouter;
+import com.quancheng.saluki.core.grpc.router.GrpcRouter;
 
 import io.grpc.ResolvedServerInfo;
 
 /**
- * @author shimingliu 2016年12月28日 下午8:40:59
- * @version ScriptRouter.java, v 0.0.1 2016年12月28日 下午8:40:59 shimingliu
+ * @author shimingliu 2016年12月15日 上午12:33:17
+ * @version Route.java, v 0.0.1 2016年12月15日 上午12:33:17 shimingliu
  */
-public class ScriptRouter implements GrpcRouter {
+public class ConditonRouter implements GrpcRouter {
 
     @Override
     public List<? extends List<ResolvedServerInfo>> router(GrpcURL url, List<ResolvedServerInfo> servers) {
