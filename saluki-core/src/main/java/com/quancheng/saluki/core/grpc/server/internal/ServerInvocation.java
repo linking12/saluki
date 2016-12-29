89ad77963ee61c448ac1ae8c24b2f9452bcbccee
刘石明 <liushiming@quancheng-ec.com>
2016-12-29 12:50:00 +0800

    add log error

--- saluki-core/src/main/java/com/quancheng/saluki/core/grpc/server/internal/ServerInvocation.java
+++ saluki-core/src/main/java/com/quancheng/saluki/core/grpc/server/internal/ServerInvocation.java
@@ -80,9 +80,7 @@ public class ServerInvocation implements UnaryMethod<Message, Message> {
         } catch (IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
             collect(reqProtoBufer, respProtoBufer, start, true);
             Throwable target = e.getCause();
-            if (log.isDebugEnabled()) {
-                log.debug(target.getMessage(), target);
-            }
+            log.error(target.getMessage(), target);
             RpcServiceException rpcBizError = new RpcServiceException(target);
             StatusRuntimeException statusException = Status.INTERNAL.withDescription(rpcBizError.getMessage())//
                                                                     .withCause(rpcBizError).asRuntimeException();
@@ -94,9 +92,7 @@ public class ServerInvocation implements UnaryMethod<Message, Message> {
                                                                     .withCause(rpcFramworkError).asRuntimeException();
             responseObserver.onError(statusException);
         } catch (Exception e) {
-            if (log.isDebugEnabled()) {
-                log.debug(e.getMessage(), e);
-            }
+            log.error(e.getMessage(), e);
             collect(reqProtoBufer, respProtoBufer, start, true);
             RpcServiceException rpcBizError = new RpcServiceException(e);
             StatusRuntimeException statusException = Status.INTERNAL.withDescription(e.getMessage())//
