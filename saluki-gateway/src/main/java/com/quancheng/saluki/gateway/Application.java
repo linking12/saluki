322e997c4fad6d939a5a4f40bf5bae56392f73b7
刘石明 <liushiming@quancheng-ec.com>
2017-03-23 21:20:42 +0800

    up

--- saluki-gateway/src/main/java/com/quancheng/saluki/gateway/Application.java
+++ saluki-gateway/src/main/java/com/quancheng/saluki/gateway/Application.java
@@ -3,11 +3,9 @@ package com.quancheng.saluki.gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 
 import com.quancheng.saluki.gateway.storage.EnableZuulProxyStore;
 
-@EnableAuthorizationServer
 @EnableZuulProxy
 @EnableZuulProxyStore
 @SpringCloudApplication
