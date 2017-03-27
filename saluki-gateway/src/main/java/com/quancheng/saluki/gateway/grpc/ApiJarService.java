fedd5ba14af96c3b4c55ad50bb568197677ec5f2
刘石明 <liushiming@quancheng-ec.com>
2017-03-27 19:05:18 +0800

    up

---
+++ saluki-gateway/src/main/java/com/quancheng/saluki/gateway/grpc/ApiJarService.java
@@ -0,0 +1,106 @@
+/*
+ * Copyright (c) 2017, Quancheng-ec.com All right reserved. This software is the
+ * confidential and proprietary information of Quancheng-ec.com ("Confidential
+ * Information"). You shall not disclose such Confidential Information and shall
+ * use it only in accordance with the terms of the license agreement you entered
+ * into with Quancheng-ec.com.
+ */
+package com.quancheng.saluki.gateway.grpc;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Date;
+
+import javax.annotation.PostConstruct;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import sun.misc.BASE64Encoder;
+
+/**
+ * @author shimingliu 2017年3月27日 下午6:55:37
+ * @version ApiJarService.java, v 0.0.1 2017年3月27日 下午6:55:37 shimingliu
+ */
+@Service
+@SuppressWarnings("restriction")
+public class ApiJarService {
+
+    private static final Logger logger       = LoggerFactory.getLogger(ApiJarService.class);
+
+    @Autowired
+    private ApiJarRepository    jarRespository;
+
+    private static final String AUTHOR       = new BASE64Encoder().encode(("liushiming:Hello899").getBytes());
+
+    private String              API_DIR_PATH = System.getProperty("user.home") + "/saluki";
+
+    private String              API_JAR_PATH;
+
+    @PostConstruct
+    public void init() {
+        API_JAR_PATH = API_DIR_PATH + File.separator + "api.jar";
+    }
+
+    public Boolean saveJar(String jarVersion, String jarUrl) {
+        ApiJar apiJar = new ApiJar();
+        apiJar.setJarUrl(jarUrl);
+        apiJar.setJarVersion(jarVersion);
+        apiJar.setCreateTime(new Date());
+        try {
+            downloadApiJar(jarUrl);
+            jarRespository.save(apiJar);
+            return true;
+        } catch (IOException e) {
+            return false;
+        }
+    }
+
+    public String getApiJarPath() {
+        return this.API_JAR_PATH;
+    }
+
+    private void downloadApiJar(String urlStr) throws IOException {
+        URL url = new URL(urlStr);
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.setConnectTimeout(3 * 1000);
+        conn.setRequestProperty("User-Agent", "Mozilla/4.0 (compatible; MSIE 5.0; Windows NT; DigExt)");
+        conn.setRequestProperty("Authorization", "Basic " + AUTHOR);
+        InputStream inputStream = conn.getInputStream();
+        byte[] getData = readInputStream(inputStream);
+        File saveDir = new File(API_DIR_PATH);
+        if (!saveDir.exists()) {
+            saveDir.mkdir();
+        }
+        File file = new File(API_JAR_PATH);
+        file.deleteOnExit();
+        FileOutputStream fos = new FileOutputStream(file);
+        fos.write(getData);
+        if (fos != null) {
+            fos.close();
+        }
+        if (inputStream != null) {
+            inputStream.close();
+        }
+        logger.info(url + " download success");
+    }
+
+    private byte[] readInputStream(InputStream inputStream) throws IOException {
+        byte[] buffer = new byte[1024];
+        int len = 0;
+        ByteArrayOutputStream bos = new ByteArrayOutputStream();
+        while ((len = inputStream.read(buffer)) != -1) {
+            bos.write(buffer, 0, len);
+        }
+        bos.close();
+        return bos.toByteArray();
+    }
+
+}
--- saluki-gateway/src/main/java/com/quancheng/saluki/gateway/grpc/GrpcRemoteComponent.java
+++ saluki-gateway/src/main/java/com/quancheng/saluki/gateway/grpc/GrpcRemoteComponent.java
@@ -7,21 +7,12 @@
  */
 package com.quancheng.saluki.gateway.grpc;
 
-import java.io.ByteArrayOutputStream;
 import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
 import java.lang.reflect.Method;
-import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLClassLoader;
-import java.util.List;
 import java.util.Map;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.TimeUnit;
 
 import javax.annotation.PostConstruct;
 
@@ -33,99 +24,35 @@ import org.springframework.stereotype.Component;
 import com.google.common.collect.Maps;
 import com.google.gson.Gson;
 import com.quancheng.saluki.boot.SalukiReference;
-import com.quancheng.saluki.core.common.NamedThreadFactory;
 import com.quancheng.saluki.core.grpc.service.GenericService;
 import com.quancheng.saluki.core.utils.ReflectUtils;
 
-import sun.misc.BASE64Encoder;
-
-@SuppressWarnings("restriction")
 @Component
 public class GrpcRemoteComponent {
 
-    private static final Logger                logger              = LoggerFactory.getLogger(GrpcRemoteComponent.class);
+    private static final Logger                logger             = LoggerFactory.getLogger(GrpcRemoteComponent.class);
 
-    private static final Map<String, Class<?>> remoteServiceCache  = Maps.newConcurrentMap();
+    private static final Map<String, Class<?>> remoteServiceCache = Maps.newConcurrentMap();
 
-    private static final Gson                  GSON                = new Gson();
+    private static final Gson                  GSON               = new Gson();
 
-    private static final String                AUTHOR              = new BASE64Encoder().encode(("liushiming:Hello899").getBytes());
-
-    @SalukiReference
+    @SalukiReference(group = "default", version = "1.0.0")
     private GenericService                     genricService;
 
     @Autowired
-    private ApiJarRepository                   apiJarRepository;
-
-    private String                             API_DIR_PATH        = System.getProperty("user.home") + "/saluki";
-
-    private String                             API_JAR_PATH;
+    private ApiJarService                      apiJarService;
 
     private URLClassLoader                     urlClassLoader;
 
-    private final ScheduledExecutorService     downLoadApiExecutor = Executors.newScheduledThreadPool(1,
-                                                                                                      new NamedThreadFactory("downLoadApi",
-                                                                                                                             true));
-
     @PostConstruct
     public void init() {
-
-        API_JAR_PATH = API_DIR_PATH + File.separator + "api.jar";
-        downLoadApiExecutor.scheduleAtFixedRate(new Runnable() {
-
-            @Override
-            public void run() {
-                try {
-                    List<ApiJar> jars = apiJarRepository.findLastesJar();
-                    String jarUrl = jars.get(0).getJarUrl();
-                    downloadApiJar(jarUrl);
-                } catch (Throwable e) {
-                    logger.error(e.getMessage(), e);
-                }
-            }
-        }, 0, 1, TimeUnit.DAYS);
+        String apiJarPath = apiJarService.getApiJarPath();
         try {
-            URL jarUrl = new File(API_JAR_PATH).toURI().toURL();
+            URL jarUrl = new File(apiJarPath).toURI().toURL();
             urlClassLoader = new URLClassLoader(new URL[] { jarUrl });
         } catch (MalformedURLException e) {
             logger.error(e.getMessage(), e);
         }
-    }
-
-    private void downloadApiJar(String urlStr) throws IOException {
-        URL url = new URL(urlStr);
-        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-        conn.setConnectTimeout(3 * 1000);
-        conn.setRequestProperty("User-Agent", "Mozilla/4.0 (compatible; MSIE 5.0; Windows NT; DigExt)");
-        conn.setRequestProperty("Authorization", "Basic " + AUTHOR);
-        InputStream inputStream = conn.getInputStream();
-        byte[] getData = readInputStream(inputStream);
-        File saveDir = new File(API_DIR_PATH);
-        if (!saveDir.exists()) {
-            saveDir.mkdir();
-        }
-        File file = new File(API_JAR_PATH);
-        file.deleteOnExit();
-        FileOutputStream fos = new FileOutputStream(file);
-        fos.write(getData);
-        if (fos != null) {
-            fos.close();
-        }
-        if (inputStream != null) {
-            inputStream.close();
-        }
-        logger.info(url + " download success");
-    }
-
-    private byte[] readInputStream(InputStream inputStream) throws IOException {
-        byte[] buffer = new byte[1024];
-        int len = 0;
-        ByteArrayOutputStream bos = new ByteArrayOutputStream();
-        while ((len = inputStream.read(buffer)) != -1) {
-            bos.write(buffer, 0, len);
-        }
-        bos.close();
-        return bos.toByteArray();
     }
 
     public Object callRemoteService(String serviceName, String group, String version, String methodName,
