b881933108b7a53aff2a0f97b50e6366cdff7ece
刘石明 <liushiming@quancheng-ec.com>
2017-03-27 18:14:42 +0800

    up

--- saluki-gateway/src/main/java/com/quancheng/saluki/gateway/grpc/GrpcRemoteComponent.java
+++ saluki-gateway/src/main/java/com/quancheng/saluki/gateway/grpc/GrpcRemoteComponent.java
@@ -28,7 +28,6 @@ import javax.annotation.PostConstruct;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import com.google.common.collect.Maps;
