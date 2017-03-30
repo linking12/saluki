8af0e7223fa4bb847c841232e0846f9c87dc784f
刘石明 <liushiming@quancheng-ec.com>
2017-03-30 21:28:41 +0800

    refector code

--- saluki-gateway/src/main/java/com/quancheng/saluki/gateway/GateWayLaucherApplication.java
+++ saluki-gateway/src/main/java/com/quancheng/saluki/gateway/GateWayLaucherApplication.java
@@ -6,10 +6,10 @@ import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @EnableZuulProxy
 @SpringCloudApplication
-public class Application {
+public class GateWayLaucherApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        SpringApplication.run(GateWayLaucherApplication.class, args);
     }
 
 }
