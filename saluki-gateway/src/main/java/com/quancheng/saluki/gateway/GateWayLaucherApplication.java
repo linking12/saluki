5cc3039656d8e07f1b38d3a7ac5fa80e109412cd
刘石明 <liushiming@quancheng-ec.com>
2017-03-31 13:12:42 +0800

    zuul for oauth2

--- saluki-gateway/src/main/java/com/quancheng/saluki/gateway/GateWayLaucherApplication.java
+++ saluki-gateway/src/main/java/com/quancheng/saluki/gateway/GateWayLaucherApplication.java
@@ -3,8 +3,12 @@ package com.quancheng.saluki.gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 @EnableZuulProxy
+@EnableResourceServer
+@EnableAuthorizationServer
 @SpringCloudApplication
 public class GateWayLaucherApplication {
 
