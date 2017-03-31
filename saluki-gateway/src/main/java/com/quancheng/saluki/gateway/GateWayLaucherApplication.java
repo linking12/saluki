19dc4ad7bd685a03844ebdc5958ea2b015d7f4cf
刘石明 <liushiming@quancheng-ec.com>
2017-03-31 16:00:51 +0800

    support spring cloud @EnableDiscoveryClient

--- saluki-gateway/src/main/java/com/quancheng/saluki/gateway/GateWayLaucherApplication.java
+++ saluki-gateway/src/main/java/com/quancheng/saluki/gateway/GateWayLaucherApplication.java
@@ -1,10 +1,10 @@
 package com.quancheng.saluki.gateway;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
-@SpringBootApplication
+@SpringCloudApplication
 @EnableZuulProxy
 public class GateWayLaucherApplication {
 
