76fe384ce29ae6b00f647ededb8a5ce7e5e65908
刘石明 <liushiming@quancheng-ec.com>
2017-04-06 17:46:02 +0800

    delete resources

--- saluki-gateway/src/main/resources/static/resources_old/js/site.js
+++
@@ -1,8 +0,0 @@
-$(function () {
-    $('[data-toggle="tooltip"]').tooltip();
-    $('*[data-remove-confirm]').on('click', function(event){
-        if(!confirm($(this).attr('data-remove-confirm'))) {
-            event.preventDefault();
-        }
-    });
-})
