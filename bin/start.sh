59cd5dbfb92b0d8e5dbd3306798c2d8e1b0d1a4c
刘石明 <liushiming@quancheng-ec.com>
2017-04-10 14:19:43 +0800

    add pinpoint

--- bin/start.sh
+++ bin/start.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+LOCAL_IP=`ifconfig eth0|grep inet|grep -v 127.0.0.1|grep -v inet6|awk '{print $2}'|tr -d "addr:"`
 JAVA_OPTS="-server -Xss256k $JAVA_OPTS"
 JAVA_OPTS="${JAVA_OPTS} -XX:SurvivorRatio=10"
 JAVA_OPTS="${JAVA_OPTS} -XX:+UseConcMarkSweepGC  -XX:CMSMaxAbortablePrecleanTime=5000 -XX:+CMSClassUnloadingEnabled -XX:CMSInitiatingOccupancyFraction=80"
@@ -9,5 +10,5 @@ JAVA_OPTS="${JAVA_OPTS} -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=/root/l
 JAVA_OPTS="${JAVA_OPTS} -Djava.awt.headless=true"
 JAVA_OPTS="${JAVA_OPTS} -Dsun.net.client.defaultConnectTimeout=10000"
 JAVA_OPTS="${JAVA_OPTS} -Dsun.net.client.defaultReadTimeout=30000"
-JAVA_OPTS="${JAVA_OPTS} -Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.port=1099 -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false"
-java -Djava.security.egd=file:/dev/./urandom $JAVA_OPTS -jar ./app.jar  $*
+JAVA_OPTS="${JAVA_OPTS} -javaagent:/root/pinpoint-agent-1.6.1-SNAPSHOT/pinpoint-bootstrap-1.6.1-SNAPSHOT.jar -Dpinpoint.agentId=${APP_NAME}-$LOCAL_IP -Dpinpoint.applicationName=${APP_NAME}"
+java -Djava.security.egd=file:/dev/./urandom $JAVA_OPTS -jar ./app.jar
