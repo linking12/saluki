5a0898388c914b40bde7f8c6324c335e374cfdd2
刘石明 <liushiming@quancheng-ec.com>
2017-04-12 17:14:26 +0800

    up

--- bin/start.sh
+++ bin/start.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 LOCAL_IP=`ifconfig eth0|grep inet|grep -v 127.0.0.1|grep -v inet6|awk '{print $2}'|tr -d "addr:"`
 JAVA_OPTS="-server -Xss256k $JAVA_OPTS"
-JAVA_OPTS="${JAVA_OPTS} -javaagent:/root/pinpoint-agent-1.6.1-SNAPSHOT/pinpoint-bootstrap-1.6.1-SNAPSHOT.jar -Dpinpoint.agentId=${APP_NAME}-$LOCAL_IP -Dpinpoint.applicationName=${APP_NAME}"
+JAVA_OPTS="${JAVA_OPTS} -javaagent:/root/pinpoint-agent-1.6.1-SNAPSHOT/pinpoint-bootstrap-1.6.1-SNAPSHOT.jar -Dpinpoint.agentId=saluki_gateway-$LOCAL_IP -Dpinpoint.applicationName=saluki_gateway"
 JAVA_OPTS="${JAVA_OPTS} -XX:SurvivorRatio=10"
 JAVA_OPTS="${JAVA_OPTS} -XX:+UseConcMarkSweepGC  -XX:CMSMaxAbortablePrecleanTime=5000 -XX:+CMSClassUnloadingEnabled -XX:CMSInitiatingOccupancyFraction=80"
 JAVA_OPTS="${JAVA_OPTS} -XX:+UseCMSInitiatingOccupancyOnly"
