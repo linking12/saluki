3d017ba85f23a490538881e73d8893e871053df6
shiming.liu <297442500@qq.com>
2017-04-26 19:02:53 +0800

    up

--- saluki-core/src/main/java/com/quancheng/saluki/core/grpc/client/hystrix/GrpcHystrixCommand.java
+++ saluki-core/src/main/java/com/quancheng/saluki/core/grpc/client/hystrix/GrpcHystrixCommand.java
@@ -36,7 +36,7 @@ public abstract class GrpcHystrixCommand extends HystrixObservableCommand<Messag
                     .andCommandPropertiesDefaults(HystrixCommandProperties.Setter().withCircuitBreakerRequestVolumeThreshold(20)// 10秒钟内至少19此请求失败，熔断器才发挥起作用
                                                                           .withCircuitBreakerSleepWindowInMilliseconds(30000)// 熔断器中断请求30秒后会进入半打开状态,放部分流量过去重试
                                                                           .withCircuitBreakerErrorThresholdPercentage(50)// 错误率达到50开启熔断保护
-                                                                          .withExecutionTimeoutEnabled(false)));// 禁用这里的超时
+                                                                          .withExecutionTimeoutEnabled(false)));// 禁用hystrix的超时
 
     }
 }
