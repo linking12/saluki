dd18682e3aaf74e16000a9059ffda45a651b7b90
liushiming <liushiming@dianrong.com>
2017-05-17 17:39:28 +0800

    delete share api add example api

--- saluki-example/saluki-example-client/src/main/java/com/quancheng/saluki/example/client/proxy/ProxyServiceController.java
+++ saluki-example/saluki-example-client/src/main/java/com/quancheng/saluki/example/client/proxy/ProxyServiceController.java
@@ -8,36 +8,26 @@ import com.quancheng.examples.model.hello.HelloRequest;
 import com.quancheng.examples.service.HelloService;
 import com.quancheng.saluki.boot.SalukiReference;
 import com.quancheng.saluki.core.common.RpcContext;
-import com.quancheng.test.model.user.UserCreateRequest;
-import com.quancheng.test.model.user.UserCreateResponse;
 
 @RestController
 @RequestMapping("/proxy")
 public class ProxyServiceController {
 
-    @SalukiReference(retries = 3)
-    private HelloService                           helloService;
+  @SalukiReference(retries = 3)
+  private HelloService helloService;
 
-    @SalukiReference
-    private com.quancheng.test.service.UserService userService;
 
-    @RequestMapping("/hello")
-    public HelloReply hello() {
-        return call();
-    }
+  @RequestMapping("/hello")
+  public HelloReply hello() {
+    return call();
+  }
 
-    private HelloReply call() {
-        HelloRequest request = new HelloRequest();
-        request.setName("liushiming");
-        RpcContext.getContext().set("123", "helloworld");
-        HelloReply reply = helloService.sayHello(request);
-        return reply;
-    }
+  private HelloReply call() {
+    HelloRequest request = new HelloRequest();
+    request.setName("liushiming");
+    RpcContext.getContext().set("123", "helloworld");
+    HelloReply reply = helloService.sayHello(request);
+    return reply;
+  }
 
-    @RequestMapping("/user")
-    public UserCreateResponse user() {
-        UserCreateRequest request = new UserCreateRequest();
-        UserCreateResponse reply = userService.create(request);
-        return reply;
-    }
 }
