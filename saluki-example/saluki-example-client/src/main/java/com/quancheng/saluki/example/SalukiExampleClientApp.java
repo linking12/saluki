8c969a27542abfa78c3dea85c2e0f2bb6eb9aed1
liushiming <liushiming@dianrong.com>
2017-05-24 13:39:30 +0800

    refector code

--- saluki-example/saluki-example-client/src/main/java/com/quancheng/saluki/example/SalukiExampleClientApp.java
+++ saluki-example/saluki-example-client/src/main/java/com/quancheng/saluki/example/SalukiExampleClientApp.java
@@ -1,17 +1,10 @@
 package com.quancheng.saluki.example;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.quancheng.examples.model.hello.HelloReply;
-import com.quancheng.examples.model.hello.HelloRequest;
-import com.quancheng.examples.service.HelloService;
-import com.quancheng.saluki.boot.SalukiReference;
-import com.quancheng.saluki.core.common.RpcContext;
-
 @SpringBootApplication
-public class SalukiExampleClientApp implements CommandLineRunner {
+public class SalukiExampleClientApp {
 
 
   public static void main(String[] args) {
@@ -19,14 +12,5 @@ public class SalukiExampleClientApp implements CommandLineRunner {
     SpringApplication.run(SalukiExampleClientApp.class, args);
   }
 
-  @Override
-  public void run(String... args) throws Exception {
-    // HelloRequest request = new HelloRequest();
-    // request.setName("liushiming");
-    // RpcContext.getContext().set("123", "helloworld");
-    // HelloReply reply = helloService.sayHello(request);
-    // System.out.println(reply);
-
-  }
 
 }
