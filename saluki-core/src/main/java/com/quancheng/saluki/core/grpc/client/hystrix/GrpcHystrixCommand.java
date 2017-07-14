0ce6a7aa12fed4228787615d57720d0ac4813970
liushiming <liushiming@dianrong.com>
2017-07-14 16:42:21 +0800

    refector hystrix

--- saluki-core/src/main/java/com/quancheng/saluki/core/grpc/client/hystrix/GrpcHystrixCommand.java
+++ saluki-core/src/main/java/com/quancheng/saluki/core/grpc/client/hystrix/GrpcHystrixCommand.java
@@ -1,19 +1,23 @@
 /*
  * Copyright 1999-2012 DianRong.
- *  
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *  
- *      http://www.apache.org/licenses/LICENSE-2.0
- *  
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
  */
 package com.quancheng.saluki.core.grpc.client.hystrix;
+
+import java.net.InetSocketAddress;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.google.protobuf.Message;
 import com.netflix.hystrix.HystrixCommand;
@@ -21,7 +25,17 @@ import com.netflix.hystrix.HystrixCommandGroupKey;
 import com.netflix.hystrix.HystrixCommandKey;
 import com.netflix.hystrix.HystrixCommandProperties;
 import com.netflix.hystrix.HystrixThreadPoolProperties;
+import com.quancheng.saluki.core.common.Constants;
 import com.quancheng.saluki.core.common.GrpcURL;
+import com.quancheng.saluki.core.common.RpcContext;
+import com.quancheng.saluki.core.grpc.client.GrpcRequest;
+import com.quancheng.saluki.core.grpc.client.GrpcResponse;
+import com.quancheng.saluki.core.grpc.client.failover.GrpcClientCall;
+import com.quancheng.saluki.core.grpc.exception.RpcFrameworkException;
+import com.quancheng.saluki.core.grpc.service.ClientServerMonitor;
+import com.quancheng.saluki.core.grpc.service.MonitorService;
+import com.quancheng.saluki.core.grpc.util.MethodDescriptorUtil;
+import com.quancheng.saluki.serializer.exception.ProtobufException;
 
 import io.grpc.MethodDescriptor;
 
@@ -29,24 +43,138 @@ import io.grpc.MethodDescriptor;
  * @author liushiming 2017年4月26日 下午6:16:32
  * @version $Id: GrpcHystrixObservableCommand.java, v 0.0.1 2017年4月26日 下午6:16:32 liushiming
  */
-public abstract class GrpcHystrixCommand extends HystrixCommand<Message> {
+public abstract class GrpcHystrixCommand extends HystrixCommand<Object> {
 
-    private static final int DEFAULT_THREADPOOL_CORE_SIZE = 5;
+  private static final Logger logger = LoggerFactory.getLogger(GrpcHystrixCommand.class);
 
-    public GrpcHystrixCommand(GrpcURL refUrl, MethodDescriptor<Message, Message> methodDesc){
-        super(Setter.withGroupKey(HystrixCommandGroupKey.Factory.asKey(refUrl.getServiceInterface()))//
-                    .andCommandKey(HystrixCommandKey.Factory.asKey(methodDesc.getFullMethodName()))//
-                    .andCommandPropertiesDefaults(HystrixCommandProperties.Setter().withCircuitBreakerRequestVolumeThreshold(20)// 10秒钟内至少19此请求失败，熔断器才发挥起作用
-                                                                          .withCircuitBreakerSleepWindowInMilliseconds(30000)// 熔断器中断请求30秒后会进入半打开状态,放部分流量过去重试
-                                                                          .withCircuitBreakerErrorThresholdPercentage(50)// 错误率达到50开启熔断保护
-                                                                          .withExecutionTimeoutEnabled(false))// 禁用这里的超时
-                    .andThreadPoolPropertiesDefaults(HystrixThreadPoolProperties.Setter().withCoreSize(getThreadPoolCoreSize(refUrl)))// 线程池为5
-        );
+  private static final int DEFAULT_THREADPOOL_CORE_SIZE = 5;
 
+  private final String serviceName;
+
+  private final String methodName;
+
+  private final long start;
+
+  private GrpcRequest request;
+
+  private GrpcClientCall clientCall;
+
+  private ClientServerMonitor clientServerMonitor;
+
+  private AtomicInteger concurrent;
+
+  public GrpcHystrixCommand(String serviceName, String methodName) {
+    super(Setter.withGroupKey(HystrixCommandGroupKey.Factory.asKey(serviceName))//
+        .andCommandKey(HystrixCommandKey.Factory.asKey(serviceName + ":" + methodName))//
+        .andCommandPropertiesDefaults(
+            HystrixCommandProperties.Setter().withCircuitBreakerRequestVolumeThreshold(20)// 10秒钟内至少19此请求失败，熔断器才发挥起作用
+                .withCircuitBreakerSleepWindowInMilliseconds(30000)// 熔断器中断请求30秒后会进入半打开状态,放部分流量过去重试
+                .withCircuitBreakerErrorThresholdPercentage(50)// 错误率达到50开启熔断保护
+                .withExecutionTimeoutEnabled(false))// 禁用这里的超时
+        .andThreadPoolPropertiesDefaults(
+            HystrixThreadPoolProperties.Setter().withCoreSize(DEFAULT_THREADPOOL_CORE_SIZE))// 线程池为5
+    );
+    this.serviceName = serviceName;
+    this.methodName = methodName;
+    this.start = System.currentTimeMillis();
+  }
+
+  public void setRequest(GrpcRequest request) {
+    this.request = request;
+  }
+
+  public void setClientCall(GrpcClientCall clientCall) {
+    this.clientCall = clientCall;
+  }
+
+  public void setClientServerMonitor(ClientServerMonitor clientServerMonitor) {
+    this.clientServerMonitor = clientServerMonitor;
+  }
+
+  public void setConcurrent(AtomicInteger concurrent) {
+    this.concurrent = concurrent;
+  }
+
+  @Override
+  protected Object run() throws Exception {
+    MethodDescriptor<Message, Message> methodDesc = this.request.getMethodDescriptor();
+    Integer timeOut = this.request.getMethodRequest().getCallTimeout();
+    Message request = getRequestMessage();
+    Message response = this.run0(request, methodDesc, timeOut, clientCall);
+    Object obj = this.transformMessage(response);
+    collect(serviceName, methodName, request, response, false);
+    return obj;
+  }
+
+  @Override
+  protected Object getFallback() {
+    Class<?> responseType = this.request.getMethodRequest().getResponseType();
+    Message response = MethodDescriptorUtil.buildDefaultInstance(responseType);
+    Object obj = this.transformMessage(response);
+    collect(serviceName, methodName, getRequestMessage(), response, true);
+    return obj;
+  }
+
+
+  private Message getRequestMessage() {
+    try {
+      return this.request.getRequestArg();
+    } catch (ProtobufException e) {
+      RpcFrameworkException rpcFramwork = new RpcFrameworkException(e);
+      throw rpcFramwork;
     }
+  }
 
-    private static int getThreadPoolCoreSize(GrpcURL refUrl) {
-        return DEFAULT_THREADPOOL_CORE_SIZE;
-
+  private Object transformMessage(Message message) {
+    Class<?> respPojoType = request.getMethodRequest().getResponseType();
+    GrpcResponse response = new GrpcResponse.Default(message, respPojoType);
+    try {
+      return response.getResponseArg();
+    } catch (ProtobufException e) {
+      RpcFrameworkException rpcFramwork = new RpcFrameworkException(e);
+      throw rpcFramwork;
     }
+  }
+
+  private void collect(String serviceName, String methodName, Message request, Message response,
+      boolean error) {
+    try {
+      Object provider = clientCall.getAffinity().get(GrpcClientCall.GRPC_CURRENT_ADDR_KEY);
+      if (request == null || response == null || provider == null) {
+        return;
+      }
+      long elapsed = System.currentTimeMillis() - this.start; // 计算调用耗时
+      int concurrent = this.concurrent.get(); // 当前并发数
+      String service = serviceName; // 获取服务名称
+      String method = methodName; // 获取方法名
+      GrpcURL refUrl = this.request.getRefUrl();
+      String host = refUrl.getHost();
+      Integer port = refUrl.getPort();
+      clientServerMonitor.collect(new GrpcURL(Constants.MONITOR_PROTOCOL, host, port, //
+          service + "/" + method, //
+          MonitorService.TIMESTAMP, String.valueOf(start), //
+          MonitorService.APPLICATION, refUrl.getParameter(Constants.APPLICATION_NAME), //
+          MonitorService.INTERFACE, service, //
+          MonitorService.METHOD, method, //
+          MonitorService.PROVIDER, ((InetSocketAddress) provider).getHostName(), //
+          error ? MonitorService.FAILURE : MonitorService.SUCCESS, "1", //
+          MonitorService.ELAPSED, String.valueOf(elapsed), //
+          MonitorService.CONCURRENT, String.valueOf(concurrent), //
+          MonitorService.INPUT, String.valueOf(request.getSerializedSize()), //
+          MonitorService.OUTPUT, String.valueOf(response.getSerializedSize())));
+    } catch (Throwable t) {
+      logger.warn("Failed to monitor count service " + serviceName + ", cause: " + t.getMessage());
+    }
+  }
+
+  protected abstract Message run0(Message req, MethodDescriptor<Message, Message> methodDesc,
+      Integer timeOut, GrpcClientCall clientCall);
+
+  protected void cacheCurrentServer() {
+    Object obj = clientCall.getAffinity().get(GrpcClientCall.GRPC_CURRENT_ADDR_KEY);
+    if (obj != null) {
+      InetSocketAddress currentServer = (InetSocketAddress) obj;
+      RpcContext.getContext().setAttachment(Constants.REMOTE_ADDRESS, currentServer.getHostName());
+    }
+  }
 }
--- saluki-core/src/main/java/com/quancheng/saluki/core/grpc/client/internal/AbstractClientInvocation.java
+++ saluki-core/src/main/java/com/quancheng/saluki/core/grpc/client/internal/AbstractClientInvocation.java
@@ -1,15 +1,13 @@
 /*
- * Copyright (c) 2016, Quancheng-ec.com All right reserved. This software is the
- * confidential and proprietary information of Quancheng-ec.com ("Confidential
- * Information"). You shall not disclose such Confidential Information and shall
- * use it only in accordance with the terms of the license agreement you entered
- * into with Quancheng-ec.com.
+ * Copyright (c) 2016, Quancheng-ec.com All right reserved. This software is the confidential and
+ * proprietary information of Quancheng-ec.com ("Confidential Information"). You shall not disclose
+ * such Confidential Information and shall use it only in accordance with the terms of the license
+ * agreement you entered into with Quancheng-ec.com.
  */
 package com.quancheng.saluki.core.grpc.client.internal;
 
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
-import java.net.InetSocketAddress;
 import java.util.Map;
 import java.util.concurrent.ConcurrentMap;
 import java.util.concurrent.atomic.AtomicInteger;
@@ -18,23 +16,16 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.google.common.collect.Maps;
-import com.google.protobuf.Message;
 import com.quancheng.saluki.core.common.Constants;
-import com.quancheng.saluki.core.common.GrpcURL;
-import com.quancheng.saluki.core.common.RpcContext;
 import com.quancheng.saluki.core.grpc.client.GrpcRequest;
-import com.quancheng.saluki.core.grpc.client.GrpcResponse;
 import com.quancheng.saluki.core.grpc.client.failover.GrpcClientCall;
 import com.quancheng.saluki.core.grpc.client.hystrix.GrpcBlockingUnaryCommand;
 import com.quancheng.saluki.core.grpc.client.hystrix.GrpcFutureUnaryCommand;
-import com.quancheng.saluki.core.grpc.exception.RpcFrameworkException;
+import com.quancheng.saluki.core.grpc.client.hystrix.GrpcHystrixCommand;
 import com.quancheng.saluki.core.grpc.service.ClientServerMonitor;
-import com.quancheng.saluki.core.grpc.service.MonitorService;
 import com.quancheng.saluki.core.utils.ReflectUtils;
-import com.quancheng.saluki.serializer.exception.ProtobufException;
 
 import io.grpc.Channel;
-import io.grpc.MethodDescriptor;
 
 /**
  * @author shimingliu 2016年12月14日 下午9:38:34
@@ -42,144 +33,88 @@ import io.grpc.MethodDescriptor;
  */
 public abstract class AbstractClientInvocation implements InvocationHandler {
 
-    private static final Logger                        log         = LoggerFactory.getLogger(AbstractClientInvocation.class);
+  private static final Logger log = LoggerFactory.getLogger(AbstractClientInvocation.class);
 
-    private final Map<String, Integer>                 methodRetries;
+  private final Map<String, Integer> methodRetries;
 
-    private final ConcurrentMap<String, AtomicInteger> concurrents = Maps.newConcurrentMap();
+  private final ConcurrentMap<String, AtomicInteger> concurrents = Maps.newConcurrentMap();
 
-    private volatile ClientServerMonitor               clientServerMonitor;
+  private volatile ClientServerMonitor clientServerMonitor;
 
-    private volatile GrpcURL                           refUrl;
+  public AbstractClientInvocation(Map<String, Integer> methodRetries) {
+    this.methodRetries = methodRetries;
+  }
 
-    private volatile Map<String, Object>               attributes;
+  protected abstract GrpcRequest buildGrpcRequest(Method method, Object[] args);
 
-    public AbstractClientInvocation(Map<String, Integer> methodRetries){
-        this.methodRetries = methodRetries;
+  @Override
+  public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+    if (ReflectUtils.isToStringMethod(method)) {
+      return AbstractClientInvocation.this.toString();
     }
-
-    protected abstract GrpcRequest buildGrpcRequest(Method method, Object[] args);
-
-    @Override
-    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
-        if (ReflectUtils.isToStringMethod(method)) {
-            return AbstractClientInvocation.this.toString();
-        }
-        GrpcRequest request = buildGrpcRequest(method, args);
-        refUrl = request.getRefUrl();
-        // 准备Grpc参数begin
-        String serviceName = request.getServiceName();
-        String methodName = request.getMethodRequest().getMethodName();
-        Message reqProtoBufer = null;
-        Message respProtoBufer = null;
-        MethodDescriptor<Message, Message> methodDesc = request.getMethodDescriptor();
-        int timeOut = request.getMethodRequest().getCallTimeout();
-        // 准备Grpc调用参数end
-        Channel channel = request.getChannel();
-        Integer retryOption = createRetryOption(methodName);
-        GrpcClientCall clientCall = GrpcClientCall.create(channel, retryOption, refUrl);
-        attributes = clientCall.getAffinity();
-        long start = System.currentTimeMillis();
-        getConcurrent(serviceName, methodName).incrementAndGet();
-        try {
-            reqProtoBufer = request.getRequestArg();
-            switch (request.getMethodRequest().getCallType()) {
-                case Constants.RPCTYPE_ASYNC:
-                    respProtoBufer = new GrpcFutureUnaryCommand(clientCall, refUrl, methodDesc, reqProtoBufer,
-                                                                timeOut).execute();
-                    break;
-                case Constants.RPCTYPE_BLOCKING:
-                    respProtoBufer = new GrpcBlockingUnaryCommand(clientCall, refUrl, methodDesc,
-                                                                  reqProtoBufer).execute();
-                    break;
-                default:
-                    respProtoBufer = new GrpcFutureUnaryCommand(clientCall, refUrl, methodDesc, reqProtoBufer,
-                                                                timeOut).execute();
-                    break;
-            }
-            Class<?> respPojoType = request.getMethodRequest().getResponseType();
-            GrpcResponse response = new GrpcResponse.Default(respProtoBufer, respPojoType);
-            Object respPojo = response.getResponseArg();
-            collect(serviceName, methodName, reqProtoBufer, respProtoBufer, start, false);
-            return respPojo;
-        } catch (ProtobufException e) {
-            collect(serviceName, methodName, reqProtoBufer, respProtoBufer, start, true);
-            RpcFrameworkException rpcFramwork = new RpcFrameworkException(e);
-            throw rpcFramwork;
-        } finally {
-            log.info(String.format("Service: %s  Method: %s  RemoteAddress: %s", serviceName, methodName,
-                                   getProviderServer()));
-            request.returnChannel(channel);
-            getConcurrent(serviceName, methodName).decrementAndGet();
-        }
+    GrpcRequest request = this.buildGrpcRequest(method, args);
+    if (clientServerMonitor == null) {
+      clientServerMonitor = new ClientServerMonitor(request.getRefUrl());
     }
-
-    public InetSocketAddress getProviderServer() {
-        InetSocketAddress currentServer = (InetSocketAddress) attributes.get(GrpcClientCall.GRPC_CURRENT_ADDR_KEY);
-        RpcContext.getContext().setAttachment(Constants.REMOTE_ADDRESS, String.valueOf(currentServer));
-        return currentServer;
+    String serviceName = request.getServiceName();
+    String methodName = request.getMethodRequest().getMethodName();
+    Channel channel = request.getChannel();
+    Integer retryOption = this.buildRetryOption(methodName);
+    GrpcClientCall clientCall = GrpcClientCall.create(channel, retryOption, request.getRefUrl());
+    try {
+      this.calculateConcurrent(serviceName, methodName).incrementAndGet();
+      AtomicInteger concurrent = this.calculateConcurrent(serviceName, methodName);
+      GrpcHystrixCommand hystrixCommand = null;
+      switch (request.getMethodRequest().getCallType()) {
+        case Constants.RPCTYPE_ASYNC:
+          hystrixCommand = new GrpcFutureUnaryCommand(serviceName, methodName);
+          break;
+        case Constants.RPCTYPE_BLOCKING:
+          hystrixCommand = new GrpcBlockingUnaryCommand(serviceName, methodName);
+          break;
+        default:
+          hystrixCommand = new GrpcFutureUnaryCommand(serviceName, methodName);
+          break;
+      }
+      hystrixCommand.setClientCall(clientCall);
+      hystrixCommand.setRequest(request);
+      hystrixCommand.setConcurrent(concurrent);
+      hystrixCommand.setClientServerMonitor(clientServerMonitor);
+      return hystrixCommand.execute();
+    } finally {
+      Object remote = clientCall.getAffinity().get(GrpcClientCall.GRPC_CURRENT_ADDR_KEY);
+      log.info(String.format("Service: %s  Method: %s  RemoteAddress: %s", serviceName, methodName,
+          String.valueOf(remote)));
+      request.returnChannel(channel);
+      this.calculateConcurrent(serviceName, methodName).decrementAndGet();
     }
+  }
 
-    private Integer createRetryOption(String methodName) {
-        if (methodRetries.size() == 1 && methodRetries.containsKey("*")) {
-            Integer retries = methodRetries.get("*");
-            if (retries != null) {
-                return retries;
-            } else {
-                return 0;
-            }
-        } else {
-            Integer retries = methodRetries.get(methodName);
-            if (retries != null) {
-                return retries;
-            } else {
-                return 0;
-            }
-        }
+  private Integer buildRetryOption(String methodName) {
+    if (methodRetries.size() == 1 && methodRetries.containsKey("*")) {
+      Integer retries = methodRetries.get("*");
+      if (retries != null) {
+        return retries;
+      } else {
+        return 0;
+      }
+    } else {
+      Integer retries = methodRetries.get(methodName);
+      if (retries != null) {
+        return retries;
+      } else {
+        return 0;
+      }
     }
+  }
 
-    private void collect(String serviceName, String methodName, Message request, Message response, long start,
-                         boolean error) {
-        try {
-            String provider = getProviderServer().getHostName();
-            if (request == null || response == null) {
-                return;
-            }
-            long elapsed = System.currentTimeMillis() - start; // 计算调用耗时
-            int concurrent = getConcurrent(serviceName, methodName).get(); // 当前并发数
-            String service = serviceName; // 获取服务名称
-            String method = methodName; // 获取方法名
-            String host = refUrl.getHost();
-            Integer port = refUrl.getPort();
-            if (clientServerMonitor == null) {
-                clientServerMonitor = new ClientServerMonitor(refUrl);
-            }
-            clientServerMonitor.collect(new GrpcURL(Constants.MONITOR_PROTOCOL, host, port, //
-                                                    service + "/" + method, //
-                                                    MonitorService.TIMESTAMP, String.valueOf(start), //
-                                                    MonitorService.APPLICATION,
-                                                    refUrl.getParameter(Constants.APPLICATION_NAME), //
-                                                    MonitorService.INTERFACE, service, //
-                                                    MonitorService.METHOD, method, //
-                                                    MonitorService.PROVIDER, provider, //
-                                                    error ? MonitorService.FAILURE : MonitorService.SUCCESS, "1", //
-                                                    MonitorService.ELAPSED, String.valueOf(elapsed), //
-                                                    MonitorService.CONCURRENT, String.valueOf(concurrent), //
-                                                    MonitorService.INPUT, String.valueOf(request.getSerializedSize()), //
-                                                    MonitorService.OUTPUT,
-                                                    String.valueOf(response.getSerializedSize())));
-        } catch (Throwable t) {
-            log.warn("Failed to monitor count service " + serviceName + ", cause: " + t.getMessage());
-        }
+  private AtomicInteger calculateConcurrent(String servcieName, String methodName) {
+    String key = servcieName + ":" + methodName;
+    AtomicInteger concurrent = concurrents.get(key);
+    if (concurrent == null) {
+      concurrents.putIfAbsent(key, new AtomicInteger());
+      concurrent = concurrents.get(key);
     }
-
-    private AtomicInteger getConcurrent(String servcieName, String methodName) {
-        String key = servcieName + "." + methodName;
-        AtomicInteger concurrent = concurrents.get(key);
-        if (concurrent == null) {
-            concurrents.putIfAbsent(key, new AtomicInteger());
-            concurrent = concurrents.get(key);
-        }
-        return concurrent;
-    }
+    return concurrent;
+  }
 }
