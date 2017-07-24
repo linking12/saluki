09d83fa30e83f83578d438a6648acfca58df9d8d
liushiming <liushiming@dianrong.com>
2017-07-24 18:40:14 +0800

    update

--- saluki-core/src/main/java/com/quancheng/saluki/core/grpc/client/GrpcClassLoader.java
+++ saluki-core/src/main/java/com/quancheng/saluki/core/grpc/client/GrpcClassLoader.java
@@ -1,9 +1,8 @@
 /*
- * Copyright (c) 2016, Quancheng-ec.com All right reserved. This software is the
- * confidential and proprietary information of Quancheng-ec.com ("Confidential
- * Information"). You shall not disclose such Confidential Information and shall
- * use it only in accordance with the terms of the license agreement you entered
- * into with Quancheng-ec.com.
+ * Copyright (c) 2016, Quancheng-ec.com All right reserved. This software is the confidential and
+ * proprietary information of Quancheng-ec.com ("Confidential Information"). You shall not disclose
+ * such Confidential Information and shall use it only in accordance with the terms of the license
+ * agreement you entered into with Quancheng-ec.com.
  */
 package com.quancheng.saluki.core.grpc.client;
 
@@ -22,90 +21,88 @@ import com.google.common.collect.Sets;
  */
 public class GrpcClassLoader extends URLClassLoader {
 
-    private static final String API_REPOSITORY = System.getProperty("user.home") + File.separator + "saluki";
+  private static final String API_REPOSITORY =
+      System.getProperty("user.home") + File.separator + "saluki";
 
-    private Set<URL>            cachedJarUrls  = Sets.newConcurrentHashSet();
+  private Set<URL> cachedJarUrls = Sets.newConcurrentHashSet();
 
-    private ClassLoader         systemClassLoader;
+  private ClassLoader systemClassLoader;
 
-    public GrpcClassLoader(){
-        super(new URL[] {}, null);
-        this.addURL();
-    }
+  public GrpcClassLoader() {
+    super(new URL[] {}, null);
+    this.addURL();
+  }
 
-    private void addURL() {
-        File file = new File(API_REPOSITORY);
-        if (file.exists() && file.isDirectory()) {
-            String[] jars = file.list(new FilenameFilter() {
+  private void addURL() {
+    File file = new File(API_REPOSITORY);
+    if (file.exists() && file.isDirectory()) {
+      String[] jars = file.list(new FilenameFilter() {
 
-                @Override
-                public boolean accept(File dir, String name) {
-                    if (name.endsWith(".jar")) {
-                        return true;
-                    }
-                    return false;
-                }
-            });
-            for (int i = 0; i < jars.length; i++) {
-                try {
-                    URL url = new File(file.getAbsolutePath(), jars[i]).toURI().toURL();
-                    if (cachedJarUrls.contains(url)) {
-                        continue;
-                    }
-                    cachedJarUrls.add(url);
-                    super.addURL(url);
-                } catch (MalformedURLException e) {
-                    // igore
-                }
-            }
+        @Override
+        public boolean accept(File dir, String name) {
+          if (name.endsWith(".jar")) {
+            return true;
+          }
+          return false;
         }
+      });
+      for (int i = 0; i < jars.length; i++) {
+        try {
+          URL url = new File(file.getAbsolutePath(), jars[i]).toURI().toURL();
+          if (cachedJarUrls.contains(url)) {
+            continue;
+          }
+          cachedJarUrls.add(url);
+          super.addURL(url);
+        } catch (MalformedURLException e) {
+          // igore
+        }
+      }
+    }
+  }
+
+  @Override
+  protected synchronized Class<?> loadClass(String name, boolean resolve)
+      throws ClassNotFoundException {
+    Class<?> clazz = null;
+    clazz = resolveClassPath(name, resolve);
+    if (clazz == null) {
+      clazz = resolveSystemClassLoader(name, resolve);
+    }
+    if (clazz == null) {
+      throw new ClassNotFoundException(name);
+    }
+    return clazz;
+  }
+
+  private Class<?> resolveClassPath(String name, boolean resolve) {
+    try {
+      Class<?> clazz = super.loadClass(name, resolve);
+      return clazz;
+    } catch (ClassNotFoundException ex) {
+      // Ignore.
     }
 
-    @Override
-    protected synchronized Class<?> loadClass(String name, boolean resolve) throws ClassNotFoundException {
-        Class<?> clazz = null;
-        if (name.startsWith("com.quancheng")) {
-            clazz = resolveClassPath(name, resolve);
-            if (clazz == null) {
-                clazz = resolveSystemClassLoader(name, resolve);
-            }
-            if (clazz == null) {
-                throw new ClassNotFoundException(name);
-            }
-        } else {
-            clazz = resolveSystemClassLoader(name, resolve);
+    return null;
+  }
+
+  private Class<?> resolveSystemClassLoader(String name, boolean resolve) {
+    if (systemClassLoader != null) {
+      try {
+        Class<?> clazz = systemClassLoader.loadClass(name);
+        if (resolve) {
+          resolveClass(clazz);
         }
         return clazz;
+      } catch (ClassNotFoundException ex) {
+        // Ignore.
+      }
     }
 
-    private Class<?> resolveClassPath(String name, boolean resolve) {
-        try {
-            Class<?> clazz = super.loadClass(name, resolve);
-            return clazz;
-        } catch (ClassNotFoundException ex) {
-            // Ignore.
-        }
+    return null;
+  }
 
-        return null;
-    }
-
-    private Class<?> resolveSystemClassLoader(String name, boolean resolve) {
-        if (systemClassLoader != null) {
-            try {
-                Class<?> clazz = systemClassLoader.loadClass(name);
-                if (resolve) {
-                    resolveClass(clazz);
-                }
-                return clazz;
-            } catch (ClassNotFoundException ex) {
-                // Ignore.
-            }
-        }
-
-        return null;
-    }
-
-    public void setSystemClassLoader(ClassLoader classLoader) {
-        this.systemClassLoader = classLoader;
-    }
+  public void setSystemClassLoader(ClassLoader classLoader) {
+    this.systemClassLoader = classLoader;
+  }
 }
