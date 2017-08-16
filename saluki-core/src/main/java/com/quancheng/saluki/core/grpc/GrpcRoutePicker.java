d9b8e3f9333fbcb53c02076d0694cf7a6be2a3d2
shiming.liu <shiming.liu@dianrong.com>
2017-08-16 19:13:31 +0800

    support stream

--- saluki-core/src/main/java/com/quancheng/saluki/core/grpc/GrpcRoutePicker.java
+++ saluki-core/src/main/java/com/quancheng/saluki/core/grpc/GrpcRoutePicker.java
@@ -20,7 +20,7 @@ import java.util.NoSuchElementException;
 
 import com.google.common.collect.Lists;
 import com.quancheng.saluki.core.common.GrpcURL;
-import com.quancheng.saluki.core.grpc.client.internal.unary.GrpcUnaryClientCall;
+import com.quancheng.saluki.core.grpc.client.internal.GrpcCallOptions;
 import com.quancheng.saluki.core.grpc.router.GrpcRouter;
 import com.quancheng.saluki.core.grpc.router.GrpcRouterFactory;
 
@@ -55,11 +55,11 @@ public class GrpcRoutePicker extends SubchannelPicker {
   @Override
   public PickResult pickSubchannel(PickSubchannelArgs args) {
     Map<String, Object> affinity =
-        args.getCallOptions().getOption(GrpcUnaryClientCall.CALLOPTIONS_CUSTOME_KEY);
-    GrpcURL refUrl = (GrpcURL) affinity.get(GrpcUnaryClientCall.GRPC_REF_URL);
+        args.getCallOptions().getOption(GrpcCallOptions.CALLOPTIONS_CUSTOME_KEY);
+    GrpcURL refUrl = (GrpcURL) affinity.get(GrpcCallOptions.GRPC_REF_URL);
     if (size > 0) {
       Subchannel subchannel = nextSubchannel(refUrl);
-      affinity.put(GrpcUnaryClientCall.GRPC_NAMERESOVER_ATTRIBUTES, nameResovleCache);
+      affinity.put(GrpcCallOptions.GRPC_NAMERESOVER_ATTRIBUTES, nameResovleCache);
       return PickResult.withSubchannel(subchannel);
     }
     if (status != null) {
