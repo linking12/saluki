b0b12daef0c99efee10b3f23c57cc4e795fcef58
shiming.liu <shiming.liu@dianrong.com>
2017-10-23 16:14:13 +0800

    performance

--- saluki-serializer/src/main/java/com/quancheng/saluki/serializer/utils/JReflectionUtils.java
+++ saluki-serializer/src/main/java/com/quancheng/saluki/serializer/utils/JReflectionUtils.java
@@ -4,119 +4,136 @@ import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.WeakHashMap;
 
 public class JReflectionUtils {
+  private static final Map<String, Map<String, Method>> METHOD_FIELD_MAP_CACHE =
+      Collections.synchronizedMap(new WeakHashMap<String, Map<String, Method>>());
 
-    private JReflectionUtils(){
+  private JReflectionUtils() {}
+
+  public static Object runGetter(Object object, Field field)
+      throws IllegalAccessException, IllegalArgumentException, InvocationTargetException {
+    final Class<?> clazz = object.getClass();
+    final String fieldName = field.getName();
+    Map<String, Method> fieldMethodMap =
+        METHOD_FIELD_MAP_CACHE.get(object.getClass().getCanonicalName());
+    if (fieldMethodMap != null) {
+      final Method fieldMethod = fieldMethodMap.get(fieldName);
+      if (fieldMethod != null) {
+        return fieldMethod.invoke(object);
+      }
+    } else {
+      fieldMethodMap = new HashMap<>();
+    }
+    try {
+      final Method method = clazz
+          .getMethod(JStringUtils.GET + JStringUtils.upperCaseFirst(fieldName), new Class<?>[] {});
+      fieldMethodMap.put(fieldName, method);
+      METHOD_FIELD_MAP_CACHE.put(object.getClass().getCanonicalName(), fieldMethodMap);
+      return method.invoke(object);
+    } catch (Exception e) {
+      // Swallow exception so that we loop through the rest.
+    }
+    for (Method method : clazz.getMethods()) {
+      final String methodName = method.getName();
+      if (((methodName.startsWith(JStringUtils.GET))
+          && (methodName.length() == (fieldName.length() + JStringUtils.GET.length())))
+          || ((methodName.startsWith(JStringUtils.IS))
+              && (methodName.length() == (fieldName.length() + JStringUtils.IS.length())))) {
+        if (methodName.toLowerCase().endsWith(fieldName.toLowerCase())) {
+          fieldMethodMap.put(fieldName, method);
+          METHOD_FIELD_MAP_CACHE.put(object.getClass().getCanonicalName(), fieldMethodMap);
+          return method.invoke(object);
+        }
+      }
+    }
+    return null;
+  }
+
+  public static Object runSetter(Object object, String method, Object arg,
+      Class<? extends Object> argClazz) throws JException {
+    try {
+      if (argClazz == null) {
+        argClazz = arg.getClass();
+      }
+      final Method m = object.getClass().getMethod(method, argClazz);
+      return m.invoke(object, arg);
+    } catch (Exception e) {
+      throw new JException(e);
+    }
+  }
+
+  public static Object runMethod(Object object, String method, Object... args) throws JException {
+    try {
+      final Method m = object.getClass().getMethod(method);
+      return m.invoke(object, args);
+    } catch (Exception e) {
+      throw new JException(e);
+    }
+  }
+
+  /**
+   * 执行静态方法
+   */
+  public static Object runStaticMethod(Class<?> clzz, String method, Object... args)
+      throws JException {
+    try {
+      Class<?>[] parameterTypes = null;
+      if (args.length != 0) {
+        parameterTypes = new Class[args.length];
+        for (int i = 0; i < args.length; i++) {
+          parameterTypes[i] = args[i].getClass();
+        }
+      }
+      Method m;
+      if (parameterTypes == null) {
+        m = clzz.getMethod(method);
+      } else {
+        m = clzz.getMethod(method, parameterTypes);
+      }
+      return m.invoke(null, args);
+    } catch (Exception e) {
+      throw new JException(e);
+    }
+  }
+
+  public static List<Field> getAllFields(List<Field> fields, Class<?> clazz) {
+    for (Field field : clazz.getDeclaredFields()) {
+      fields.add(field);
     }
 
-    public static Object runGetter(Object object, Field field) throws IllegalAccessException, IllegalArgumentException,
-                                                               InvocationTargetException {
-        final Class<?> clazz = object.getClass();
-        final String fieldName = field.getName();
-        try {
-            final Method method = clazz.getMethod(JStringUtils.GET + JStringUtils.upperCaseFirst(fieldName),
-                                                  new Class<?>[] {});
-            return method.invoke(object);
-        } catch (Exception e) {
-            // Swallow exception so that we loop through the rest.
-        }
-        for (Method method : clazz.getMethods()) {
-            final String methodName = method.getName();
-            if (((methodName.startsWith(JStringUtils.GET))
-                 && (methodName.length() == (fieldName.length() + JStringUtils.GET.length())))
-                || ((methodName.startsWith(JStringUtils.IS))
-                    && (methodName.length() == (fieldName.length() + JStringUtils.IS.length())))) {
-                if (methodName.toLowerCase().endsWith(fieldName.toLowerCase())) {
-                    return method.invoke(object);
-                }
-            }
-        }
-
-        return null;
+    if (clazz.getSuperclass() != null) {
+      fields = getAllFields(fields, clazz.getSuperclass());
     }
 
-    public static Object runSetter(Object object, String method, Object arg,
-                                   Class<? extends Object> argClazz) throws JException {
-        try {
-            if (argClazz == null) {
-                argClazz = arg.getClass();
-            }
-            final Method m = object.getClass().getMethod(method, argClazz);
+    return fields;
+  }
 
-            return m.invoke(object, arg);
-        } catch (Exception e) {
-            throw new JException(e);
-        }
+  public static List<Method> getAllMethods(List<Method> methods, Class<?> clazz) {
+    for (Method method : clazz.getDeclaredMethods()) {
+      methods.add(method);
     }
 
-    public static Object runMethod(Object object, String method, Object... args) throws JException {
-        try {
-            final Method m = object.getClass().getMethod(method);
-
-            return m.invoke(object, args);
-        } catch (Exception e) {
-            throw new JException(e);
-        }
+    if (clazz.getSuperclass() != null) {
+      methods = getAllMethods(methods, clazz.getSuperclass());
     }
 
-    /**
-     * 执行静态方法
-     */
-    public static Object runStaticMethod(Class<?> clzz, String method, Object... args) throws JException {
-        try {
-            Class<?>[] parameterTypes = null;
-            if (args.length != 0) {
-                parameterTypes = new Class[args.length];
-                for (int i = 0; i < args.length; i++) {
-                    parameterTypes[i] = args[i].getClass();
-                }
-            }
-            Method m;
-            if (parameterTypes == null) {
-                m = clzz.getMethod(method);
-            } else {
-                m = clzz.getMethod(method, parameterTypes);
-            }
-            return m.invoke(null, args);
-        } catch (Exception e) {
-            throw new JException(e);
-        }
+    return methods;
+  }
+
+  public static Method getMethodByName(Class<?> clazz, String name) {
+    final List<Method> methods = JReflectionUtils.getAllMethods(new ArrayList<Method>(), clazz);
+
+    for (Method method : methods) {
+      if (method.getName().equals(name)) {
+        return method;
+      }
     }
-
-    public static List<Field> getAllFields(List<Field> fields, Class<?> clazz) {
-        for (Field field : clazz.getDeclaredFields()) {
-            fields.add(field);
-        }
-
-        if (clazz.getSuperclass() != null) {
-            fields = getAllFields(fields, clazz.getSuperclass());
-        }
-
-        return fields;
-    }
-
-    public static List<Method> getAllMethods(List<Method> methods, Class<?> clazz) {
-        for (Method method : clazz.getDeclaredMethods()) {
-            methods.add(method);
-        }
-
-        if (clazz.getSuperclass() != null) {
-            methods = getAllMethods(methods, clazz.getSuperclass());
-        }
-
-        return methods;
-    }
-
-    public static Method getMethodByName(Class<?> clazz, String name) {
-        final List<Method> methods = JReflectionUtils.getAllMethods(new ArrayList<Method>(), clazz);
-
-        for (Method method : methods) {
-            if (method.getName().equals(name)) {
-                return method;
-            }
-        }
-        return null;
-    }
+    return null;
+  }
 }
--- saluki-serializer/src/main/java/com/quancheng/saluki/serializer/utils/JStringUtils.java
+++ saluki-serializer/src/main/java/com/quancheng/saluki/serializer/utils/JStringUtils.java
@@ -17,172 +17,173 @@ import org.apache.commons.lang3.StringUtils;
 
 public final class JStringUtils {
 
-    public static final String  EMPTY         = StringUtils.EMPTY;
+  public static final String EMPTY = StringUtils.EMPTY;
 
-    public static final String  WILD_CARD     = "*";
+  public static final String WILD_CARD = "*";
 
-    public static final String  UNDERSCORE    = "_";
+  public static final String UNDERSCORE = "_";
 
-    public static final String  HYPHEN        = "-";
+  public static final String HYPHEN = "-";
 
-    public static final String  BACKSLASH     = "\\";
+  public static final String BACKSLASH = "\\";
 
-    public static final String  QUESTION_MARK = "?";
+  public static final String QUESTION_MARK = "?";
 
-    public static final String  NEW_LINE      = "\n";
+  public static final String NEW_LINE = "\n";
 
-    public static final String  TAB           = "\t";
+  public static final String TAB = "\t";
 
-    public static final String  SHORT_NO      = "N";
+  public static final String SHORT_NO = "N";
 
-    public static final String  SHORT_YES     = "Y";
+  public static final String SHORT_YES = "Y";
 
-    public static final String  PERCENT       = "%";
+  public static final String PERCENT = "%";
 
-    public static final String  FORWARD_SLASH = "/";
+  public static final String FORWARD_SLASH = "/";
 
-    public static final String  PLUS          = "+";
+  public static final String PLUS = "+";
 
-    public static final String  COMMA         = ",";
+  public static final String COMMA = ",";
 
-    public static final String  PERIOD        = ".";
+  public static final String PERIOD = ".";
 
-    public static final String  GET           = "get";
+  public static final String GET = "get";
 
-    public static final String  IS            = "is";
+  public static final String IS = "is";
 
-    public static final Charset UTF8_CHARSET  = Charset.forName("UTF-8");
+  public static final Charset UTF8_CHARSET = Charset.forName("UTF-8");
 
-    private JStringUtils(){
-        // empty utility class
+  private JStringUtils() {
+    // empty utility class
+  }
+
+  public static String safeTrim(String str) {
+    if (str == null) {
+      return EMPTY;
+    }
+    return str.trim();
+  }
+
+  public static String ltrim(String source) {
+    return source.replaceAll("^\\s+", EMPTY);
+  }
+
+  public static String rtrim(String source) {
+    return source.replaceAll("\\s+$", EMPTY);
+  }
+
+  public static boolean hasDigit(String str) {
+    final Pattern pattern = Pattern.compile("[0-9]");
+    final Matcher matcher = pattern.matcher(str);
+    while (matcher.find()) {
+      return true;
+    }
+    return false;
+  }
+
+  public static String pad(String rawString, int length, char padChar) {
+    String padded = rawString;
+    String padFiller = null;
+
+    if (padded.length() == length) {
+      return padded;
     }
 
-    public static String safeTrim(String str) {
-        if (str == null) {
-            return EMPTY;
-        }
-        return str.trim();
+    if (padded.length() < length) {
+      //
+      // Keep a zero string that is big enough to pad the
+      // largest field that we have, then to pad each field
+      // we just append this string to the field and then
+      // substr to the desired size.
+      //
+      StringBuffer buff = new StringBuffer(length);
+
+      for (int i = buff.length(); i < length; i++) {
+        buff.append(padChar);
+      }
+      padFiller = buff.toString();
+
+      padded = rawString + padFiller;
+      return padded.substring(0, length);
+
+    } else {
+      return padded.substring(0, length);
+    }
+  }
+
+  public static String convertInputStreamToString(InputStream is, String charset)
+      throws IOException {
+    if (is == null) {
+      return EMPTY;
     }
 
-    public static String ltrim(String source) {
-        return source.replaceAll("^\\s+", EMPTY);
+    final StringBuilder sb = new StringBuilder();
+    BufferedReader reader = null;
+    try {
+      if (charset != null) {
+        reader = new BufferedReader(new InputStreamReader(is, charset));
+      } else {
+        reader = new BufferedReader(new InputStreamReader(is));
+      }
+
+      String line;
+      while ((line = reader.readLine()) != null) {
+        sb.append(line);
+      }
+    } finally {
+      if (reader != null) {
+        reader.close();
+      }
     }
 
-    public static String rtrim(String source) {
-        return source.replaceAll("\\s+$", EMPTY);
+    return sb.toString();
+  }
+
+  public static String upperCaseFirst(String str) {
+    if (str == null || str.equals(EMPTY)) {
+      return str;
     }
 
-    public static boolean hasDigit(String str) {
-        final Pattern pattern = Pattern.compile("[0-9]");
-        final Matcher matcher = pattern.matcher(str);
-        while (matcher.find()) {
-            return true;
-        }
-        return false;
+    return Character.toUpperCase(str.charAt(0)) + str.substring(1);
+  }
+
+  public static String join(Collection<?> collection, String separator) {
+    return StringUtils.join(collection, separator);
+  }
+
+  public static List<String> split(String str, String separatorChars) {
+    final String[] array = StringUtils.split(str, separatorChars);
+
+    return Arrays.asList(array);
+  }
+
+  public static String decode(byte[] bytes, Charset charset) {
+    return new String(bytes, charset);
+  }
+
+  public static byte[] encode(String string, Charset charset) {
+    return string.getBytes(charset);
+  }
+
+  public static boolean isNullOrEmpty(@Nullable String string) {
+    if (string == null || string.isEmpty()) {
+      return true;
     }
+    return false;
+  }
 
-    public static String pad(String rawString, int length, char padChar) {
-        String padded = rawString;
-        String padFiller = null;
+  public static boolean hasValue(String string) {
+    return !isNullOrEmpty(string);
+  }
 
-        if (padded.length() == length) {
-            return padded;
-        }
+  public static boolean compare(@Nullable String str1, @Nullable String str2) {
+    return (str1 == null ? str2 == null : str1.equals(str2));
+  }
 
-        if (padded.length() < length) {
-            //
-            // Keep a zero string that is big enough to pad the
-            // largest field that we have, then to pad each field
-            // we just append this string to the field and then
-            // substr to the desired size.
-            //
-            StringBuffer buff = new StringBuffer(length);
-
-            for (int i = buff.length(); i < length; i++) {
-                buff.append(padChar);
-            }
-            padFiller = buff.toString();
-
-            padded = rawString + padFiller;
-            return padded.substring(0, length);
-
-        } else {
-            return padded.substring(0, length);
-        }
+  public static String getNonNullValue(String string) {
+    if (string == null) {
+      return JStringUtils.EMPTY;
     }
-
-    public static String convertInputStreamToString(InputStream is, String charset) throws IOException {
-        if (is == null) {
-            return EMPTY;
-        }
-
-        final StringBuilder sb = new StringBuilder();
-        BufferedReader reader = null;
-        try {
-            if (charset != null) {
-                reader = new BufferedReader(new InputStreamReader(is, charset));
-            } else {
-                reader = new BufferedReader(new InputStreamReader(is));
-            }
-
-            String line;
-            while ((line = reader.readLine()) != null) {
-                sb.append(line);
-            }
-        } finally {
-            if (reader != null) {
-                reader.close();
-            }
-        }
-
-        return sb.toString();
-    }
-
-    public static String upperCaseFirst(String str) {
-        if (str == null || str.equals(EMPTY)) {
-            return str;
-        }
-
-        return Character.toUpperCase(str.charAt(0)) + str.substring(1);
-    }
-
-    public static String join(Collection<?> collection, String separator) {
-        return StringUtils.join(collection, separator);
-    }
-
-    public static List<String> split(String str, String separatorChars) {
-        final String[] array = StringUtils.split(str, separatorChars);
-
-        return Arrays.asList(array);
-    }
-
-    public static String decode(byte[] bytes, Charset charset) {
-        return new String(bytes, charset);
-    }
-
-    public static byte[] encode(String string, Charset charset) {
-        return string.getBytes(charset);
-    }
-
-    public static boolean isNullOrEmpty(@Nullable String string) {
-        if (string == null || string.isEmpty()) {
-            return true;
-        }
-        return false;
-    }
-
-    public static boolean hasValue(String string) {
-        return !isNullOrEmpty(string);
-    }
-
-    public static boolean compare(@Nullable String str1, @Nullable String str2) {
-        return (str1 == null ? str2 == null : str1.equals(str2));
-    }
-
-    public static String getNonNullValue(String string) {
-        if (string == null) {
-            return JStringUtils.EMPTY;
-        }
-        return string;
-    }
+    return string;
+  }
 }
